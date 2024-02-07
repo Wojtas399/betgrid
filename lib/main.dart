9406f404f2dcdb02c4770992af7daca628ee94c3
Wojtas399 <wojtekp26@gmail.com>
2024-02-07 13:15:23 +0100

    Configured auto_route.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'dependency_injection.dart';
 import 'firebase_options.dart';
+import 'ui/config/router/app_router.dart';
 import 'ui/config/theme/theme.dart';
 import 'ui/config/theme/theme_notifier.dart';
 
@@ -14,79 +15,25 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(
-    const ProviderScope(
-      child: MyApp(),
+    ProviderScope(
+      child: _MyApp(),
     ),
   );
 }
 
-class MyApp extends ConsumerWidget {
-  const MyApp({super.key});
+class _MyApp extends ConsumerWidget {
+  final _appRouter = AppRouter();
+
+  _MyApp();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: _appRouter.config(),
       title: 'BetGrid',
       themeMode: ref.watch(themeNotifierProvider),
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      home: Scaffold(
-        appBar: _AppBar(),
-        body: const _Body(),
-      ),
-    );
-  }
-}
-
-class _AppBar extends StatelessWidget implements PreferredSizeWidget {
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      title: const Text('BetGrid'),
-      actions: const [
-        Row(
-          children: [
-            Icon(Icons.contrast),
-            _ThemeModeSwitch(),
-          ],
-        ),
-        SizedBox(width: 8),
-      ],
-    );
-  }
-}
-
-class _ThemeModeSwitch extends ConsumerWidget {
-  const _ThemeModeSwitch();
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final ThemeMode themeMode = ref.watch(themeNotifierProvider);
-
-    return Switch(
-      value: themeMode == ThemeMode.dark,
-      onChanged: (bool isSwitched) {
-        ref.read(themeNotifierProvider.notifier).changeThemeMode(
-              isSwitched ? ThemeMode.dark : ThemeMode.light,
-            );
-      },
-    );
-  }
-}
-
-class _Body extends StatelessWidget {
-  const _Body();
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'BetGrid',
-        style: Theme.of(context).textTheme.displayLarge,
-      ),
     );
   }
 }
---
+++ lib/ui/config/router/app_router.dart
@@ -0,0 +1,13 @@
+import 'package:auto_route/auto_route.dart';
+
+import '../../screen/screens.dart';
+
+part 'app_router.gr.dart';
+
+@AutoRouterConfig()
+class AppRouter extends _$AppRouter {
+  @override
+  List<AutoRoute> get routes => [
+        AutoRoute(page: SignInRoute.page, initial: true),
+      ];
+}
