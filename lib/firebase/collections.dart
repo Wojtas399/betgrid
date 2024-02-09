61e9af9133fdf1275497671e738a867bc0a4ac0b
Wojtas399 <wojtekp26@gmail.com>
2024-02-09 11:02:35 +0100

    Created FirebaseDriverService.

--- lib/firebase/collections.dart
+++ lib/firebase/collections.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import 'model/driver_dto/driver_dto.dart';
 import 'model/grand_prix_dto/grand_prix_dto.dart';
 
 CollectionReference<GrandPrixDto> getGrandPrixesRef() =>
@@ -11,4 +12,13 @@ CollectionReference<GrandPrixDto> getGrandPrixesRef() =>
             snapshot.data(),
           ),
           toFirestore: (GrandPrixDto grandPrixDto, _) => grandPrixDto.toJson(),
+        );
+
+CollectionReference<DriverDto> getDriversRef() =>
+    FirebaseFirestore.instance.collection('Drivers').withConverter<DriverDto>(
+          fromFirestore: (snapshot, _) => DriverDto.fromIdAndJson(
+            snapshot.id,
+            snapshot.data(),
+          ),
+          toFirestore: (DriverDto driverDto, _) => driverDto.toJson(),
         );
