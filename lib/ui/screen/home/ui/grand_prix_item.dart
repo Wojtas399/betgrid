188c77a2d102e5e1d52c775ebe808f120a84d0eb
Wojtas399 <wojtekp26@gmail.com>
2024-02-09 10:44:59 +0100

    New look of grand prix items.

--- lib/ui/screen/home/ui/grand_prix_item.dart
+++ lib/ui/screen/home/ui/grand_prix_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../../model/grand_prix.dart';
+import '../../../extensions/build_context_extensions.dart';
 import '../../../service/formatter_service.dart';
 
 class GrandPrixItem extends StatelessWidget {
@@ -10,20 +11,55 @@ class GrandPrixItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ExpansionTile(
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Card(
+        clipBehavior: Clip.hardEdge,
+        child: ExpansionTile(
+          shape: const Border(),
+          title: Text(
+            grandPrix.name,
+            style: textTheme.titleMedium?.copyWith(
+              color: colorScheme.primary,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          subtitle: Text(
+            '${grandPrix.startDate.toDayAndMonth()} - ${grandPrix.endDate.toDayAndMonth()}',
+            style: textTheme.bodyMedium?.copyWith(
+              color: colorScheme.outline.withOpacity(0.75),
+            ),
+          ),
+          children: [
+            _BetSection(title: context.str.qualifications),
+            _BetSection(title: context.str.race),
+            _BetSection(title: context.str.other),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _BetSection extends StatelessWidget {
+  final String title;
+
+  const _BetSection({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
       title: Text(
-        grandPrix.name,
+        title,
         style: Theme.of(context).textTheme.titleMedium,
       ),
-      subtitle: Text(
-        '${grandPrix.startDate.toDayAndMonth()} - ${grandPrix.endDate.toDayAndMonth()}',
-        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-              color: Theme.of(context).colorScheme.outline,
-            ),
-      ),
-      children: const [
+      trailing: const Icon(Icons.circle_outlined),
+      onTap: () {
         //TODO
-      ],
+      },
     );
   }
 }
