def21129ae9e0b098c99dc33ce4207105c8420c6
Wojtas399 <wojtekp26@gmail.com>
2024-02-17 12:03:04 +0100

    Firebase user service.

---
+++ lib/firebase/service/firebase_user_service.dart
@@ -0,0 +1,22 @@
+import 'package:injectable/injectable.dart';
+
+import '../collections.dart';
+import '../model/user_dto/user_dto.dart';
+
+@injectable
+class FirebaseUserService {
+  Future<UserDto?> loadUserById({required String userId}) async {
+    final snapshot = await getUsersRef().doc(userId).get();
+    return snapshot.data();
+  }
+
+  Future<UserDto?> addUser({
+    required String userId,
+    required String nick,
+  }) async {
+    final docRef = getUsersRef().doc(userId);
+    await docRef.set(UserDto(nick: nick));
+    final snapshot = await docRef.get();
+    return snapshot.data();
+  }
+}
