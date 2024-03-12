0e49995c27967bc5f943a17fa2a40152e7d9a60b
Wojtas399 <wojtekp26@gmail.com>
2024-03-12 17:26:33 +0100

    Displaying player's total points on its profile screen.

---
+++ lib/ui/screen/player_profile/player_profile_grand_prixes.dart
@@ -0,0 +1,92 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../model/grand_prix.dart';
+import '../../component/grand_prix_item_component.dart';
+import '../../component/scroll_animated_item_component.dart';
+import '../../component/text/title.dart';
+import '../../config/router/app_router.dart';
+import '../../provider/grand_prix/all_grand_prixes_provider.dart';
+import '../../provider/grand_prix_bet_points_provider.dart';
+
+class PlayerProfileGrandPrixes extends SliverPadding {
+  PlayerProfileGrandPrixes({super.key, required String playerId})
+      : super(
+          padding: const EdgeInsets.fromLTRB(24, 24, 24, 64),
+          sliver: Consumer(
+            builder: (
+              BuildContext context,
+              WidgetRef ref,
+              __,
+            ) =>
+                ref.watch(allGrandPrixesProvider).when(
+                      data: (List<GrandPrix>? grandPrixes) {
+                        final sortedGrandPrixes = [...?grandPrixes];
+                        sortedGrandPrixes.sort(
+                          (gp1, gp2) => gp1.startDate.compareTo(gp2.startDate),
+                        );
+
+                        return _GrandPrixesList(
+                          grandPrixes: sortedGrandPrixes,
+                          playerId: playerId,
+                        );
+                      },
+                      error: (_, __) => const SliverToBoxAdapter(
+                        child: Center(
+                          child: TitleMedium(
+                            'Cannot load grand prixes',
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      loading: () => const SliverToBoxAdapter(
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      ),
+                    ),
+          ),
+        );
+}
+
+class _GrandPrixesList extends SliverList {
+  _GrandPrixesList({
+    required List<GrandPrix> grandPrixes,
+    required String playerId,
+  }) : super(
+          delegate: SliverChildBuilderDelegate(
+            childCount: grandPrixes.length,
+            (_, int index) {
+              final grandPrix = grandPrixes[index];
+
+              return ScrollAnimatedItem(
+                child: Consumer(
+                  builder: (context, ref, child) {
+                    final grandPrixPoints = ref.watch(
+                      grandPrixBetPointsProvider(
+                        playerId: playerId,
+                        grandPrixId: grandPrix.id,
+                      ).select((state) => state.value?.totalPoints),
+                    );
+
+                    return GrandPrixItem(
+                      betPoints: grandPrixPoints,
+                      roundNumber: index + 1,
+                      grandPrix: grandPrix,
+                      onPressed: () {
+                        context.navigateTo(
+                          GrandPrixBetRoute(
+                            grandPrixId: grandPrix.id,
+                            playerId: playerId,
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              );
+            },
+          ),
+        );
+}
--- lib/ui/screen/player_profile/player_profile_screen.dart
+++ lib/ui/screen/player_profile/player_profile_screen.dart
@@ -2,15 +2,15 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../model/grand_prix.dart';
 import '../../../model/player.dart';
 import '../../component/avatar_component.dart';
-import '../../component/grand_prix_item_component.dart';
-import '../../component/scroll_animated_item_component.dart';
+import '../../component/gap/gap_vertical.dart';
+import '../../component/text/display.dart';
+import '../../component/text/headline.dart';
 import '../../component/text/title.dart';
-import '../../config/router/app_router.dart';
-import '../../provider/grand_prix/all_grand_prixes_provider.dart';
-import '../../provider/grand_prix_bet_points_provider.dart';
+import '../../extensions/build_context_extensions.dart';
+import '../../provider/player_points_provider.dart';
+import 'player_profile_grand_prixes.dart';
 
 @RoutePage()
 class PlayerProfileScreen extends StatelessWidget {
@@ -23,38 +23,14 @@ class PlayerProfileScreen extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          _AppBar.build(
+          _AppBar(
             backgroundColor: Theme.of(context).colorScheme.primary,
             foregroundColor: Theme.of(context).canvasColor,
             player: player,
             context: context,
           ),
-          Consumer(
-            builder: (_, ref, __) {
-              return ref.watch(allGrandPrixesProvider).when(
-                    data: (List<GrandPrix>? grandPrixes) {
-                      final sortedGrandPrixes = [...?grandPrixes];
-                      sortedGrandPrixes.sort(
-                        (gp1, gp2) => gp1.startDate.compareTo(gp2.startDate),
-                      );
-
-                      return _GrandPrixesList.build(
-                        context: context,
-                        grandPrixes: sortedGrandPrixes,
-                        playerId: player.id,
-                      );
-                    },
-                    error: (_, __) => SliverList(
-                      delegate: SliverChildListDelegate(
-                        [
-                          const Text('Cannot load grand prixes'),
-                        ],
-                      ),
-                    ),
-                    loading: () => _LoadingIndicator.build(),
-                  );
-            },
-          ),
+          _TotalPoints(playerId: player.id),
+          PlayerProfileGrandPrixes(playerId: player.id),
         ],
       ),
     );
@@ -62,115 +38,81 @@ class PlayerProfileScreen extends StatelessWidget {
 }
 
 class _AppBar extends SliverAppBar {
-  const _AppBar({
-    super.backgroundColor,
-    super.foregroundColor,
-    super.flexibleSpace,
-  }) : super(
-          pinned: true,
-          expandedHeight: 300,
-          surfaceTintColor: Colors.transparent,
-        );
-
-  factory _AppBar.build({
+  _AppBar({
     required Player player,
     required BuildContext context,
     Color? backgroundColor,
     Color? foregroundColor,
-  }) =>
-      _AppBar(
-        backgroundColor: backgroundColor,
-        foregroundColor: foregroundColor,
-        flexibleSpace: FlexibleSpaceBar(
-          title: TitleLarge(
-            player.username,
-            color: Theme.of(context).canvasColor,
-          ),
-          centerTitle: true,
-          titlePadding: const EdgeInsets.symmetric(
-            vertical: 16,
-            horizontal: 0,
-          ),
-          background: LayoutBuilder(builder: (_, BoxConstraints constraints) {
-            final double avatarSize = constraints.maxHeight * 0.55;
+  }) : super(
+          pinned: true,
+          expandedHeight: 300,
+          surfaceTintColor: Colors.transparent,
+          backgroundColor: backgroundColor,
+          foregroundColor: foregroundColor,
+          flexibleSpace: FlexibleSpaceBar(
+            title: TitleLarge(
+              player.username,
+              color: Theme.of(context).canvasColor,
+            ),
+            centerTitle: true,
+            titlePadding: const EdgeInsets.symmetric(
+              vertical: 16,
+              horizontal: 0,
+            ),
+            background: LayoutBuilder(
+              builder: (_, BoxConstraints constraints) {
+                final double avatarSize = constraints.maxHeight * 0.55;
 
-            return Center(
-              child: SizedBox(
-                width: avatarSize,
-                height: avatarSize,
-                child: Hero(
-                  tag: player.id,
-                  child: Avatar(
-                    avatarUrl: player.avatarUrl,
-                    username: player.username,
+                return Center(
+                  child: SizedBox(
+                    width: avatarSize,
+                    height: avatarSize,
+                    child: Hero(
+                      tag: player.id,
+                      child: Avatar(
+                        avatarUrl: player.avatarUrl,
+                        username: player.username,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            );
-          }),
-        ),
-      );
+                );
+              },
+            ),
+          ),
+        );
 }
 
-class _GrandPrixesList extends SliverPadding {
-  const _GrandPrixesList({required super.padding, super.sliver});
+class _TotalPoints extends SliverPadding {
+  _TotalPoints({required String playerId})
+      : super(
+          padding: const EdgeInsets.fromLTRB(24, 24, 24, 0),
+          sliver: SliverToBoxAdapter(
+            child: Builder(
+              builder: (BuildContext context) {
+                return Column(
+                  children: [
+                    HeadlineMedium(
+                      context.str.grandPrixBetPoints,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    const GapVertical8(),
+                    Consumer(
+                      builder: (_, ref, __) {
+                        final totalPoints = ref.watch(
+                          playerPointsProvider(playerId: playerId),
+                        );
 
-  factory _GrandPrixesList.build({
-    required BuildContext context,
-    required List<GrandPrix> grandPrixes,
-    required String playerId,
-  }) =>
-      _GrandPrixesList(
-        padding: const EdgeInsets.fromLTRB(24, 24, 24, 64),
-        sliver: SliverList(
-          delegate: SliverChildBuilderDelegate(
-            childCount: grandPrixes.length,
-            (_, int index) {
-              final grandPrix = grandPrixes[index];
-
-              return ScrollAnimatedItem(
-                child: Consumer(builder: (context, ref, child) {
-                  final grandPrixPoints = ref.watch(
-                    grandPrixBetPointsProvider(
-                      playerId: playerId,
-                      grandPrixId: grandPrix.id,
-                    ).select((state) => state.value?.totalPoints),
-                  );
-
-                  return GrandPrixItem(
-                    betPoints: grandPrixPoints,
-                    roundNumber: index + 1,
-                    grandPrix: grandPrix,
-                    onPressed: () {
-                      context.navigateTo(
-                        GrandPrixBetRoute(
-                          grandPrixId: grandPrix.id,
-                          playerId: playerId,
-                        ),
-                      );
-                    },
-                  );
-                }),
-              );
-            },
+                        return DisplayLarge(
+                          '${totalPoints ?? '--'}',
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).colorScheme.primary,
+                        );
+                      },
+                    ),
+                  ],
+                );
+              },
+            ),
           ),
-        ),
-      );
-}
-
-class _LoadingIndicator extends SliverPadding {
-  const _LoadingIndicator({required super.padding, super.sliver});
-
-  factory _LoadingIndicator.build() => _LoadingIndicator(
-        padding: const EdgeInsets.only(top: 32),
-        sliver: SliverList(
-          delegate: SliverChildListDelegate(
-            [
-              const Center(
-                child: CircularProgressIndicator(),
-              )
-            ],
-          ),
-        ),
-      );
+        );
 }
