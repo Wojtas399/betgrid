25f3383660a5caca18373741b5a6d95a91261c29
Wojtas399 <wojtekp26@gmail.com>
2024-03-20 13:08:57 +0100

    Improved implementation of loggedUserProvider.

--- lib/ui/provider/logged_user_provider.dart
+++ lib/ui/provider/logged_user_provider.dart
@@ -1,5 +1,4 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:rxdart/rxdart.dart';
 
 import '../../data/repository/auth/auth_repository.dart';
 import '../../data/repository/user/user_repository.dart';
@@ -8,11 +7,18 @@ import '../../model/user.dart';
 part 'logged_user_provider.g.dart';
 
 @riverpod
-Stream<User?> loggedUser(LoggedUserRef ref) =>
-    ref.watch(authRepositoryProvider).loggedUserId$.switchMap(
-          (String? loggedUserId) => loggedUserId != null
-              ? ref
-                  .watch(userRepositoryProvider)
-                  .getUserById(userId: loggedUserId)
-              : Stream.value(null),
-        );
+Stream<User?> loggedUser(LoggedUserRef ref) async* {
+  final Stream<String?> loggedUserId$ =
+      ref.watch(authRepositoryProvider).loggedUserId$;
+  await for (final loggedUserId in loggedUserId$) {
+    if (loggedUserId == null) {
+      yield null;
+    } else {
+      final Stream<User?> loggedUser$ =
+          ref.watch(userRepositoryProvider).getUserById(userId: loggedUserId);
+      await for (final loggedUser in loggedUser$) {
+        yield loggedUser;
+      }
+    }
+  }
+}
