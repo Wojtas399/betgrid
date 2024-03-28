b01829b319ddcc135d2e147e8db260dd9a8afbee
Wojtas399 <wojtekp26@gmail.com>
2024-03-28 12:22:53 +0100

    Modified grand prix results provider.

--- lib/ui/screen/grand_prix_bet/provider/grand_prix_results_provider.dart
+++ lib/ui/screen/grand_prix_bet/provider/grand_prix_results_provider.dart
@@ -1,17 +1,19 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-import '../../../../data/repository/grand_prix_result/grand_prix_results_repository.dart';
+import '../../../../data/repository/grand_prix_result/grand_prix_results_repository_method_providers.dart';
 import '../../../../model/grand_prix_results.dart';
 import '../../../provider/grand_prix/grand_prix_id_provider.dart';
 
 part 'grand_prix_results_provider.g.dart';
 
 @Riverpod(dependencies: [grandPrixId])
-Stream<GrandPrixResults?> grandPrixResults(GrandPrixResultsRef ref) {
+Future<GrandPrixResults?> grandPrixResults(GrandPrixResultsRef ref) async {
   final String? grandPrixId = ref.watch(grandPrixIdProvider);
   return grandPrixId != null
-      ? ref.watch(grandPrixResultsRepositoryProvider).getResultForGrandPrix(
+      ? await ref.watch(
+          resultsForGrandPrixProvider(
             grandPrixId: grandPrixId,
-          )
-      : Stream.value(null);
+          ).future,
+        )
+      : null;
 }
--- test/ui/screen/grand_prix_bet/grand_prix_results_provider_test.dart
+++ test/ui/screen/grand_prix_bet/grand_prix_results_provider_test.dart
@@ -1,68 +1,63 @@
-import 'package:betgrid/data/repository/grand_prix_result/grand_prix_results_repository.dart';
+import 'package:betgrid/data/repository/grand_prix_result/grand_prix_results_repository_method_providers.dart';
+import 'package:betgrid/model/grand_prix_results.dart';
 import 'package:betgrid/ui/provider/grand_prix/grand_prix_id_provider.dart';
 import 'package:betgrid/ui/screen/grand_prix_bet/provider/grand_prix_results_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
 
 import '../../../creator/grand_prix_results_creator.dart';
-import '../../../mock/data/repository/mock_grand_prix_results_repository.dart';
 
 void main() {
-  final grandPrixResultsRepository = MockGrandPrixResultsRepository();
-
-  ProviderContainer makeProviderContainer({String? grandPrixId}) {
+  ProviderContainer makeProviderContainer({
+    String? grandPrixId,
+    GrandPrixResults? grandPrixResults,
+  }) {
     final container = ProviderContainer(
       overrides: [
         grandPrixIdProvider.overrideWithValue(grandPrixId),
-        grandPrixResultsRepositoryProvider.overrideWithValue(
-          grandPrixResultsRepository,
-        ),
+        if (grandPrixId != null)
+          resultsForGrandPrixProvider(
+            grandPrixId: grandPrixId,
+          ).overrideWith(
+            (_) => Stream.value(grandPrixResults),
+          ),
       ],
     );
     addTearDown(container.dispose);
     return container;
   }
 
-  tearDown(() {
-    reset(grandPrixResultsRepository);
-  });
-
   test(
     'grand prix id is null, '
-    'should emit null',
+    'should return null',
     () async {
       final container = makeProviderContainer(grandPrixId: null);
 
-      await expectLater(
-        container.read(grandPrixResultsProvider.future),
-        completion(null),
+      final GrandPrixResults? results = await container.read(
+        grandPrixResultsProvider.future,
       );
+
+      expect(results, null);
     },
   );
 
   test(
-    'should get grand prix results from GrandPrixResultsRepository and '
-    'should emit them',
+    'should get grand prix results and should return them',
     () async {
       const String grandPrixId = 'gp1';
       final expectedResults = createGrandPrixResults(
         raceResults: createRaceResults(p1DriverId: 'd1', p2DriverId: 'd2'),
       );
-      grandPrixResultsRepository.mockGetResultsForGrandPrix(
-        results: expectedResults,
+      final container = makeProviderContainer(
+        grandPrixId: grandPrixId,
+        grandPrixResults: expectedResults,
       );
-      final container = makeProviderContainer(grandPrixId: grandPrixId);
 
-      await expectLater(
-        container.read(grandPrixResultsProvider.future),
-        completion(expectedResults),
+      final GrandPrixResults? results = await container.read(
+        grandPrixResultsProvider.future,
       );
-      verify(
-        () => grandPrixResultsRepository.getResultForGrandPrix(
-          grandPrixId: grandPrixId,
-        ),
-      ).called(1);
+
+      expect(results, expectedResults);
     },
   );
 }
