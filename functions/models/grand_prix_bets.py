992f20a995ca5f4db5004960e274a8e9e0accee2
Wojtas399 <wojtekp26@gmail.com>
2024-04-27 10:51:08 +0200

    Changed params name to snake_version in GrandPrixBets model.

--- functions/models/grand_prix_bets.py
+++ functions/models/grand_prix_bets.py
@@ -2,28 +2,42 @@ from pydantic import BaseModel
 from typing import List, Optional
 
 class GrandPrixBets(BaseModel):
-    grandPrixId: str
-    qualiStandingsByDriverIds: Optional[List[str]]
-    p1DriverId: Optional[str]
-    p2DriverId: Optional[str]
-    p3DriverId: Optional[str]
-    p10DriverId: Optional[str]
-    fastestLapDriverId: Optional[str]
-    dnfDriverIds: Optional[List[str]]
-    willBeSafetyCar: Optional[bool]
-    willBeRedFlag: Optional[bool]
+    grand_prix_id: str
+    quali_standings_by_driver_ids: Optional[List[str]]
+    p1_driver_id: Optional[str]
+    p2_driver_id: Optional[str]
+    p3_driver_id: Optional[str]
+    p10_driver_id: Optional[str]
+    fastest_lap_driver_id: Optional[str]
+    dnf_driver_ids: Optional[List[str]]
+    will_be_safety_car: Optional[bool]
+    will_be_red_flag: Optional[bool]
 
     @staticmethod
     def from_dict(source):
         return GrandPrixBets(
-            grandPrixId=source['grandPrixId'],
-            qualiStandingsByDriverIds=source['qualiStandingsByDriverIds'],
-            p1DriverId=source['p1DriverId'],
-            p2DriverId=source['p2DriverId'],
-            p3DriverId=source['p3DriverId'],
-            p10DriverId=source['p10DriverId'],
-            fastestLapDriverId=source['fastestLapDriverId'],
-            dnfDriverIds=source['dnfDriverIds'],
-            willBeSafetyCar=source['willBeSafetyCar'],
-            willBeRedFlag=source['willBeRedFlag'],
+            grand_prix_id = source['grandPrixId'],
+            quali_standings_by_driver_ids = source['qualiStandingsByDriverIds'],
+            p1_driver_id = source['p1DriverId'],
+            p2_driver_id = source['p2DriverId'],
+            p3_driver_id = source['p3DriverId'],
+            p10_driver_id = source['p10DriverId'],
+            fastest_lap_driver_id = source['fastestLapDriverId'],
+            dnf_driver_ids = source['dnfDriverIds'],
+            will_be_safety_car = source['willBeSafetyCar'],
+            will_be_red_flag = source['willBeRedFlag'],
         )
+    
+    def to_dict(self):
+        return {
+            'grandPrixId': self.grand_prix_id,
+            'qualiStandingsByDriverIds': self.quali_standings_by_driver_ids,
+            'p1DriverId': self.p1_driver_id,
+            'p2DriverId': self.p2_driver_id,
+            'p3DriverId': self.p3_driver_id,
+            'p10DriverId': self.p10_driver_id,
+            'fastestLapDriverId': self.fastest_lap_driver_id,
+            'dnfDriverIds': self.dnf_driver_ids,
+            'willBeSafetyCar': self.will_be_safety_car,
+            'willBeRedFlag': self.will_be_red_flag,
+        }
