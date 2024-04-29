67253f9ede095d0a040003186580d38a58d4bc36
Wojtas399 <wojtekp26@gmail.com>
2024-04-29 07:58:03 +0200

    Changed param names to snake_version in GrandPrixPoints model.

--- functions/models/grand_prix_points.py
+++ functions/models/grand_prix_points.py
@@ -4,7 +4,15 @@ from models.quali_bet_points import QualiBetPoints
 from models.race_bet_points import RaceBetPoints
 
 class GrandPrixPoints(BaseModel):
-    grandPrixId: str
-    qualiBetPoints: Optional[QualiBetPoints]
-    raceBetPoints: Optional[RaceBetPoints]
-    totalPoints: float
+    grand_prix_id: str
+    quali_bet_points: Optional[QualiBetPoints]
+    race_bet_points: Optional[RaceBetPoints]
+    total_points: float
+
+    def to_dict(self):
+        return {
+            'grandPrixId': self.grand_prix_id,
+            'qualiBetPoints': self.quali_bet_points.to_dict(),
+            'raceBetPoints': self.race_bet_points.to_dict(),
+            'totalPoints': self.total_points,
+        }
