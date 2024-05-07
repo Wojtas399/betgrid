96d84c2cc2ef2afa3afa555df443b5e9fcbcd120
Wojtas399 <wojtekp26@gmail.com>
2024-05-07 10:54:51 +0200

    Removed using of loggedUserIdProvider in OtherPlayers provider.

--- lib/ui/screen/players/provider/other_players_provider.dart
+++ lib/ui/screen/players/provider/other_players_provider.dart
@@ -1,14 +1,16 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-import '../../../../data/repository/auth/auth_repository_method_providers.dart';
+import '../../../../data/repository/auth/auth_repository.dart';
 import '../../../../data/repository/player/player_repository_method_providers.dart';
+import '../../../../dependency_injection.dart';
 import '../../../../model/player.dart';
 
 part 'other_players_provider.g.dart';
 
 @riverpod
 Future<List<Player>?> otherPlayers(OtherPlayersRef ref) async {
-  final String? loggedUserId = await ref.watch(loggedUserIdProvider.future);
+  final String? loggedUserId =
+      await getIt.get<AuthRepository>().loggedUserId$.first;
   if (loggedUserId == null) return null;
   final List<Player>? allPlayers = await ref.watch(allPlayersProvider.future);
   return allPlayers
--- test/ui/screen/players/other_players_provider_test.dart
+++ test/ui/screen/players/other_players_provider_test.dart
@@ -1,18 +1,22 @@
-import 'package:betgrid/data/repository/auth/auth_repository_method_providers.dart';
+import 'package:betgrid/data/repository/auth/auth_repository.dart';
 import 'package:betgrid/data/repository/player/player_repository_method_providers.dart';
 import 'package:betgrid/model/player.dart';
 import 'package:betgrid/ui/screen/players/provider/other_players_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../mock/data/repository/mock_auth_repository.dart';
 
 void main() {
+  final authRepository = MockAuthRepository();
+
   ProviderContainer makeProviderContainer({
-    String? loggedUserId,
     List<Player>? allPlayers,
   }) {
     final container = ProviderContainer(
       overrides: [
-        loggedUserIdProvider.overrideWith((_) => Stream.value(loggedUserId)),
         allPlayersProvider.overrideWith((_) => Stream.value(allPlayers)),
       ],
     );
@@ -20,10 +24,19 @@ void main() {
     return container;
   }
 
+  setUpAll(() {
+    GetIt.I.registerSingleton<AuthRepository>(authRepository);
+  });
+
+  tearDown(() {
+    reset(authRepository);
+  });
+
   test(
     'logged user id is null, '
     'should return null',
     () async {
+      authRepository.mockGetLoggedUserId(null);
       final container = makeProviderContainer();
 
       final List<Player>? otherPlayers = await container.read(
@@ -50,8 +63,8 @@ void main() {
         allPlayers[2],
         allPlayers.last,
       ];
+      authRepository.mockGetLoggedUserId(loggedUserId);
       final container = makeProviderContainer(
-        loggedUserId: loggedUserId,
         allPlayers: allPlayers,
       );
 
