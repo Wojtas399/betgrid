ae93454d00dcc12166f2211be06140d50e3f90e9
Wojtas399 <wojtekp26@gmail.com>
2024-05-07 10:58:19 +0200

    Removed using of loggedUserIdProvider in loggedUserProvider.

--- lib/ui/provider/logged_user_provider.dart
+++ lib/ui/provider/logged_user_provider.dart
@@ -1,14 +1,16 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-import '../../data/repository/auth/auth_repository_method_providers.dart';
+import '../../data/repository/auth/auth_repository.dart';
 import '../../data/repository/user/user_repository_method_providers.dart';
+import '../../dependency_injection.dart';
 import '../../model/user.dart';
 
 part 'logged_user_provider.g.dart';
 
 @riverpod
 Future<User?> loggedUser(LoggedUserRef ref) async {
-  final String? loggedUserId = await ref.watch(loggedUserIdProvider.future);
+  final String? loggedUserId =
+      await getIt.get<AuthRepository>().loggedUserId$.first;
   if (loggedUserId == null) return null;
   return await ref.watch(
     userProvider(userId: loggedUserId).future,
--- test/ui/provider/logged_user_provider_test.dart
+++ test/ui/provider/logged_user_provider_test.dart
@@ -1,14 +1,18 @@
+import 'package:betgrid/data/repository/auth/auth_repository.dart';
 import 'package:betgrid/data/repository/auth/auth_repository_method_providers.dart';
 import 'package:betgrid/data/repository/user/user_repository_method_providers.dart';
 import 'package:betgrid/model/user.dart';
 import 'package:betgrid/ui/provider/logged_user_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mocktail/mocktail.dart';
 
 import '../../creator/user_creator.dart';
+import '../../mock/data/repository/mock_auth_repository.dart';
 
 void main() {
-  const String loggedUserId = 'u1';
+  final authRepository = MockAuthRepository();
 
   ProviderContainer makeProviderContainer({
     String? loggedUserId,
@@ -27,11 +31,20 @@ void main() {
     return container;
   }
 
+  setUpAll(() {
+    GetIt.I.registerSingleton<AuthRepository>(authRepository);
+  });
+
+  tearDown(() {
+    reset(authRepository);
+  });
+
   test(
     'build, '
     'logged user id is null'
     'should emit null',
     () async {
+      authRepository.mockGetLoggedUserId(null);
       final container = makeProviderContainer();
 
       final User? loggedUser = await container.read(loggedUserProvider.future);
@@ -44,10 +57,12 @@ void main() {
     'build, '
     'should emit logged user data',
     () async {
+      const String loggedUserId = 'u1';
       final User expectedLoggedUser = createUser(
         id: loggedUserId,
         username: 'username',
       );
+      authRepository.mockGetLoggedUserId(loggedUserId);
       final container = makeProviderContainer(
         loggedUserId: loggedUserId,
         loggedUser: expectedLoggedUser,
