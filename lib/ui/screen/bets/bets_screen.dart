b083f0cec1a0ee541149d04d1f18fe2015356df4
Wojtas399 <wojtekp26@gmail.com>
2024-05-07 11:00:58 +0200

    Removed using of loggedUserIdProvider in Bets screen.

--- lib/ui/screen/bets/bets_screen.dart
+++ lib/ui/screen/bets/bets_screen.dart
@@ -2,7 +2,8 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../data/repository/auth/auth_repository_method_providers.dart';
+import '../../../data/repository/auth/auth_repository.dart';
+import '../../../dependency_injection.dart';
 import '../../component/sliver_grand_prixes_list_component.dart';
 import '../../component/sliver_player_total_points_component.dart';
 import '../../config/router/app_router.dart';
@@ -10,21 +11,25 @@ import '../../provider/grand_prixes_with_points_provider.dart';
 import '../../provider/player_points_provider.dart';
 
 @RoutePage()
-class BetsScreen extends ConsumerWidget {
+class BetsScreen extends StatelessWidget {
   const BetsScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final loggedUserId = ref.watch(loggedUserIdProvider);
+  Widget build(BuildContext context) {
+    final loggedUserId$ = getIt.get<AuthRepository>().loggedUserId$;
 
-    return loggedUserId.isLoading
-        ? const CircularProgressIndicator()
-        : CustomScrollView(
-            slivers: [
-              _TotalPoints(loggedUserId: loggedUserId.value!),
-              _GrandPrixes(loggedUserId: loggedUserId.value!),
-            ],
-          );
+    return StreamBuilder(
+      stream: loggedUserId$,
+      builder: (_, AsyncSnapshot<String?> asyncSnapshot) =>
+          asyncSnapshot.connectionState == ConnectionState.waiting
+              ? const CircularProgressIndicator()
+              : CustomScrollView(
+                  slivers: [
+                    _TotalPoints(loggedUserId: asyncSnapshot.data!),
+                    _GrandPrixes(loggedUserId: asyncSnapshot.data!),
+                  ],
+                ),
+    );
   }
 }
 
