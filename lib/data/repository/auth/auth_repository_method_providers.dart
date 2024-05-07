e1c358b0281a4f46fa42d43168506bafd6f6a487
Wojtas399 <wojtekp26@gmail.com>
2024-05-07 10:37:52 +0200

    Removed provider of AuthState.

--- lib/data/repository/auth/auth_repository_method_providers.dart
+++ lib/data/repository/auth/auth_repository_method_providers.dart
@@ -6,9 +6,5 @@ import 'auth_repository.dart';
 part 'auth_repository_method_providers.g.dart';
 
 @riverpod
-Stream<AuthState> authState(AuthStateRef ref) =>
-    ref.watch(authRepositoryProvider).authState$;
-
-@riverpod
 Stream<String?> loggedUserId(LoggedUserIdRef ref) =>
     ref.watch(authRepositoryProvider).loggedUserId$;
--- test/data/repository/auth/auth_repository_method_providers_test.dart
+++ test/data/repository/auth/auth_repository_method_providers_test.dart
@@ -1,6 +1,5 @@
 import 'package:betgrid/data/repository/auth/auth_repository.dart';
 import 'package:betgrid/data/repository/auth/auth_repository_method_providers.dart';
-import 'package:betgrid/model/auth_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -18,22 +17,6 @@ void main() {
     addTearDown(container.dispose);
     return container;
   }
-
-  test(
-    'authStateProvider, '
-    'should get auth state from AuthRepository and should emit it',
-    () async {
-      const AuthState expectedAuthState = AuthStateUserIsSignedIn();
-      authRepository.mockGetAuthState(authState: expectedAuthState);
-      final container = makeProviderContainer();
-
-      final AuthState authState = await container.read(
-        authStateProvider.future,
-      );
-
-      expect(authState, expectedAuthState);
-    },
-  );
 
   test(
     'loggedUserIdProvider, '
