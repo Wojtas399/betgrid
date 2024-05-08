570b722dc465b867e4c84e1d0872567d8d7c153a
Wojtas399 <wojtekp26@gmail.com>
2024-05-08 19:23:34 +0200

    Removed provider GrandPrixResultsRepository.

--- lib/data/repository/grand_prix_result/grand_prix_results_repository.dart
+++ lib/data/repository/grand_prix_result/grand_prix_results_repository.dart
@@ -1,23 +1,7 @@
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-import '../../../firebase/service/firebase_grand_prix_results_service.dart';
 import '../../../model/grand_prix_results.dart';
-import 'grand_prix_results_repository_impl.dart';
-
-part 'grand_prix_results_repository.g.dart';
 
 abstract interface class GrandPrixResultsRepository {
   Stream<GrandPrixResults?> getResultForGrandPrix({
     required String grandPrixId,
   });
 }
-
-@Riverpod(keepAlive: true)
-GrandPrixResultsRepository grandPrixResultsRepository(
-  GrandPrixResultsRepositoryRef ref,
-) =>
-    GrandPrixResultsRepositoryImpl(
-      ref.read(
-        firebaseGrandPrixResultsServiceProvider,
-      ),
-    );
