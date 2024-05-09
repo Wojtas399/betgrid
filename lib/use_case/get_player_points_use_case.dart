6cee1b673bf688fb56a150a8ba1b50241d42b008
Wojtas399 <wojtekp26@gmail.com>
2024-05-09 11:07:58 +0200

    Use case for getting player's points.

---
+++ lib/use_case/get_player_points_use_case.dart
@@ -0,0 +1,53 @@
+import 'package:injectable/injectable.dart';
+
+import '../data/repository/grand_prix/grand_prix_repository.dart';
+import '../data/repository/grand_prix_bet_points/grand_prix_bet_points_repository.dart';
+import '../model/grand_prix.dart';
+import '../model/grand_prix_bet_points.dart';
+
+@injectable
+class GetPlayerPointsUseCase {
+  final GrandPrixRepository _grandPrixRepository;
+  final GrandPrixBetPointsRepository _grandPrixBetPointsRepository;
+
+  const GetPlayerPointsUseCase(
+    this._grandPrixRepository,
+    this._grandPrixBetPointsRepository,
+  );
+
+  Stream<double?> call({
+    required String playerId,
+  }) async* {
+    final Stream<List<GrandPrix>?> allGrandPrixes$ =
+        _grandPrixRepository.getAllGrandPrixes();
+    await for (final allGrandPrixes in allGrandPrixes$) {
+      if (allGrandPrixes == null) {
+        yield null;
+      } else {
+        final Stream<double?> totalPoints$ =
+            _calculateTotalPoints(playerId, allGrandPrixes);
+        await for (final totalPoints in totalPoints$) {
+          yield totalPoints;
+        }
+      }
+    }
+  }
+
+  Stream<double?> _calculateTotalPoints(
+    String playerId,
+    List<GrandPrix> allGrandPrixes,
+  ) async* {
+    double totalPoints = 0.0;
+    for (final grandPrix in allGrandPrixes) {
+      final Stream<GrandPrixBetPoints?> gpPoints$ =
+          _grandPrixBetPointsRepository.getPointsForPlayerByGrandPrixId(
+        playerId: playerId,
+        grandPrixId: grandPrix.id,
+      );
+      await for (final gpPoints in gpPoints$) {
+        totalPoints += gpPoints != null ? gpPoints.totalPoints : 0;
+      }
+    }
+    yield totalPoints;
+  }
+}
