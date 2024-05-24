afa56fa3999b5ab3775501c562c12186e2acb269
Wojtas399 <wojtekp26@gmail.com>
2024-05-24 09:41:08 +0200

    Deleted unused grandPrixIdProvider.

--- lib/ui/provider/grand_prix_id_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-part 'grand_prix_id_provider.g.dart';
-
-@Riverpod(dependencies: [])
-String? grandPrixId(GrandPrixIdRef ref) => null;
