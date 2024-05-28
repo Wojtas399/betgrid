5a9aab83edccf8aa00fdbdec8bebda3c62343ece
Wojtas399 <wojtekp26@gmail.com>
2024-05-28 07:52:13 +0200

    Use case to get finished grand prixes.

---
+++ lib/use_case/get_finished_grand_prixes_use_case.dart
@@ -0,0 +1,27 @@
+import 'package:injectable/injectable.dart';
+
+import '../data/repository/grand_prix/grand_prix_repository.dart';
+import '../model/grand_prix.dart';
+import '../ui/service/date_service.dart';
+
+@injectable
+class GetFinishedGrandPrixesUseCase {
+  final GrandPrixRepository _grandPrixRepository;
+  final DateService _dateService;
+
+  const GetFinishedGrandPrixesUseCase(
+    this._grandPrixRepository,
+    this._dateService,
+  );
+
+  Stream<List<GrandPrix>> call() =>
+      _grandPrixRepository.getAllGrandPrixes().map(
+        (List<GrandPrix>? allGrandPrixes) {
+          if (allGrandPrixes == null || allGrandPrixes.isEmpty) return [];
+          final now = _dateService.getNow();
+          return allGrandPrixes
+              .where((GrandPrix gp) => gp.startDate.isBefore(now))
+              .toList();
+        },
+      );
+}
