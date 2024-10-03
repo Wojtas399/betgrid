a576abc37fd5c9bb79285639c64ee9be9a16f547
Wojtas399 <wojtekp26@gmail.com>
2024-10-03 10:56:14 +0200

    Using FirebaseCollections class within FirebaseGrandPrixBetPointsService.

--- lib/data/firebase/service/firebase_grand_prix_bet_points_service.dart
+++ lib/data/firebase/service/firebase_grand_prix_bet_points_service.dart
@@ -5,12 +5,17 @@ import '../model/grand_prix_bet_points_dto/grand_prix_bet_points_dto.dart';
 
 @injectable
 class FirebaseGrandPrixBetPointsService {
+  final FirebaseCollections _firebaseCollections;
+
+  const FirebaseGrandPrixBetPointsService(this._firebaseCollections);
+
   Future<GrandPrixBetPointsDto?>
       fetchGrandPrixBetPointsByPlayerIdAndGrandPrixId({
     required String playerId,
     required String grandPrixId,
   }) async {
-    final snapshot = await getGrandPrixBetPointsRef(playerId)
+    final snapshot = await _firebaseCollections
+        .grandPrixesBetPoints(playerId)
         .where('grandPrixId', isEqualTo: grandPrixId)
         .limit(1)
         .get();
