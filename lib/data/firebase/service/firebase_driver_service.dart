47618c80e7308afde76839e6710f170c095eaaeb
Wojtas399 <wojtekp26@gmail.com>
2024-10-03 10:54:59 +0200

    Using FirebaseCollections class withing FirebaseDriverService.

--- lib/data/firebase/service/firebase_driver_service.dart
+++ lib/data/firebase/service/firebase_driver_service.dart
@@ -5,13 +5,17 @@ import '../model/driver_dto/driver_dto.dart';
 
 @injectable
 class FirebaseDriverService {
+  final FirebaseCollections _firebaseCollections;
+
+  const FirebaseDriverService(this._firebaseCollections);
+
   Future<List<DriverDto>> fetchAllDrivers() async {
-    final snapshot = await getDriversRef().get();
+    final snapshot = await _firebaseCollections.drivers().get();
     return snapshot.docs.map((doc) => doc.data()).toList();
   }
 
   Future<DriverDto?> fetchDriverById({required String driverId}) async {
-    final snapshot = await getDriversRef().doc(driverId).get();
+    final snapshot = await _firebaseCollections.drivers().doc(driverId).get();
     return snapshot.data();
   }
 }
