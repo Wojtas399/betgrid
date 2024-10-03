aae7917a3795ffcf9011f47902f4ceacb6525afc
Wojtas399 <wojtekp26@gmail.com>
2024-10-03 10:59:07 +0200

    Using FirebaseCollections class within FirebaseGrandPrixService.

--- lib/data/firebase/service/firebase_grand_prix_service.dart
+++ lib/data/firebase/service/firebase_grand_prix_service.dart
@@ -5,15 +5,20 @@ import '../model/grand_prix_dto/grand_prix_dto.dart';
 
 @injectable
 class FirebaseGrandPrixService {
+  final FirebaseCollections _firebaseCollections;
+
+  const FirebaseGrandPrixService(this._firebaseCollections);
+
   Future<List<GrandPrixDto>> fetchAllGrandPrixes() async {
-    final snapshot = await getGrandPrixesRef().get();
+    final snapshot = await _firebaseCollections.grandPrixes().get();
     return snapshot.docs.map((doc) => doc.data()).toList();
   }
 
   Future<GrandPrixDto?> fetchGrandPrixById({
     required String grandPrixId,
   }) async {
-    final snapshot = await getGrandPrixesRef().doc(grandPrixId).get();
+    final snapshot =
+        await _firebaseCollections.grandPrixes().doc(grandPrixId).get();
     return snapshot.data();
   }
 }
