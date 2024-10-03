821ff504885178c60ebeae8f266e3d91ec548491
Wojtas399 <wojtekp26@gmail.com>
2024-10-03 11:01:34 +0200

    Deleted unused global scoped functions from collections file.

--- lib/data/firebase/collections.dart
+++ lib/data/firebase/collections.dart
@@ -12,8 +12,6 @@ import 'model/user_dto/user_dto.dart';
 class FirebaseCollections {
   const FirebaseCollections();
 
-  String get name => 'name';
-
   //TODO: Add season param
   CollectionReference<GrandPrixDto> grandPrixes() => FirebaseFirestore.instance
       .collection('GrandPrixes')
@@ -102,90 +100,3 @@ class FirebaseCollections {
             toFirestore: (GrandPrixBetPointsDto dto, _) => dto.toJson(),
           );
 }
-
-//TODO: Add season param
-CollectionReference<GrandPrixDto> getGrandPrixesRef() =>
-    FirebaseFirestore.instance
-        .collection('GrandPrixes')
-        .withConverter<GrandPrixDto>(
-          fromFirestore: (snapshot, _) {
-            final data = snapshot.data();
-            if (data == null) throw 'Grand prix document data was null';
-            return GrandPrixDto.fromFirebase(
-              id: snapshot.id,
-              season: 2024, //TODO: Pass season param here
-              json: data,
-            );
-          },
-          toFirestore: (GrandPrixDto dto, _) => dto.toJson(),
-        );
-
-CollectionReference<DriverDto> getDriversRef() =>
-    FirebaseFirestore.instance.collection('Drivers').withConverter<DriverDto>(
-          fromFirestore: (snapshot, _) {
-            final data = snapshot.data();
-            if (data == null) throw 'Driver document data was null';
-            return DriverDto.fromIdAndJson(snapshot.id, data);
-          },
-          toFirestore: (DriverDto dto, _) => dto.toJson(),
-        );
-
-CollectionReference<GrandPrixResultsDto> getGrandPrixResultsRef() =>
-    FirebaseFirestore.instance
-        .collection('GrandPrixResults')
-        .withConverter<GrandPrixResultsDto>(
-          fromFirestore: (snapshot, _) {
-            final data = snapshot.data();
-            if (data == null) throw 'Grand prix result document data was null';
-            return GrandPrixResultsDto.fromIdAndJson(snapshot.id, data);
-          },
-          toFirestore: (GrandPrixResultsDto dto, _) => dto.toJson(),
-        );
-
-CollectionReference<UserDto> getUsersRef() =>
-    FirebaseFirestore.instance.collection('Users').withConverter<UserDto>(
-          fromFirestore: (snapshot, _) {
-            final data = snapshot.data();
-            if (data == null) throw 'User document data was null';
-            return UserDto.fromIdAndJson(snapshot.id, data);
-          },
-          toFirestore: (UserDto dto, _) => dto.toJson(),
-        );
-
-CollectionReference<GrandPrixBetDto> getGrandPrixBetsRef(String userId) =>
-    FirebaseFirestore.instance
-        .collection('Users')
-        .doc(userId)
-        .collection('GrandPrixBets')
-        .withConverter<GrandPrixBetDto>(
-          fromFirestore: (snapshot, _) {
-            final data = snapshot.data();
-            if (data == null) throw 'Grand prix bet document was null';
-            return GrandPrixBetDto.fromIdPlayerIdAndJson(
-              snapshot.id,
-              userId,
-              data,
-            );
-          },
-          toFirestore: (GrandPrixBetDto dto, _) => dto.toJson(),
-        );
-
-CollectionReference<GrandPrixBetPointsDto> getGrandPrixBetPointsRef(
-  String userId,
-) =>
-    FirebaseFirestore.instance
-        .collection('Users')
-        .doc(userId)
-        .collection('GrandPrixBetPoints')
-        .withConverter<GrandPrixBetPointsDto>(
-          fromFirestore: (snapshot, _) {
-            final data = snapshot.data();
-            if (data == null) throw 'Grand prix bet points document was null';
-            return GrandPrixBetPointsDto.fromIdPlayerIdAndJson(
-              id: snapshot.id,
-              playerId: userId,
-              json: data,
-            );
-          },
-          toFirestore: (GrandPrixBetPointsDto dto, _) => dto.toJson(),
-        );
