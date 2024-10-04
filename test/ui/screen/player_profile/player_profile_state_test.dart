a463f36806a3495565cf8a92f15814ed7edc38ef
Wojtas399 <wojtekp26@gmail.com>
2024-10-04 17:35:38 +0200

    Refactored tests for PlayerProfileState.

--- test/ui/screen/player_profile/player_profile_state_test.dart
+++ test/ui/screen/player_profile/player_profile_state_test.dart
@@ -2,27 +2,30 @@ import 'package:betgrid/ui/screen/player_profile/cubit/player_profile_state.dart
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test(
-    'isLoading, '
-    'should be true if status is set as loading',
+  group(
+    'isLoading, ',
     () {
-      const state = PlayerProfileState(
-        status: PlayerProfileStateStatus.loading,
+      test(
+        'should be true if status is set as loading',
+        () {
+          const state = PlayerProfileState(
+            status: PlayerProfileStateStatus.loading,
+          );
+
+          expect(state.isLoading, true);
+        },
       );
 
-      expect(state.isLoading, true);
-    },
-  );
+      test(
+        'should be true if status is set as completed',
+        () {
+          const state = PlayerProfileState(
+            status: PlayerProfileStateStatus.completed,
+          );
 
-  test(
-    'isLoading, '
-    'should be true if status is set as completed',
-    () {
-      const state = PlayerProfileState(
-        status: PlayerProfileStateStatus.completed,
+          expect(state.isLoading, false);
+        },
       );
-
-      expect(state.isLoading, false);
     },
   );
 }
