cf4d855df03ff42b810cd7d0d2c3178278c76877
Wojtas399 <wojtekp26@gmail.com>
2024-10-04 17:52:48 +0200

    Refactored tests for ProfileCubit.

--- test/ui/screen/profile/profile_cubit_test.dart
+++ test/ui/screen/profile/profile_cubit_test.dart
@@ -24,173 +24,188 @@ void main() {
     reset(userRepository);
   });
 
-  blocTest(
-    'initialize, '
-    'logged user id is null, '
-    'should emit state with status set as loggedUserDoesNotExist',
-    build: () => createCubit(),
-    setUp: () => authRepository.mockGetLoggedUserId(null),
-    act: (cubit) async => await cubit.initialize(),
-    expect: () => [
-      const ProfileState(
-        status: ProfileStateStatus.loggedUserDoesNotExist,
-      ),
-    ],
-    verify: (_) => verify(() => authRepository.loggedUserId$).called(1),
-  );
-
-  blocTest(
-    'initialize, '
-    'should load and emit logged user data',
-    build: () => createCubit(),
-    setUp: () {
-      authRepository.mockGetLoggedUserId('u1');
-      userRepository.mockGetUserById(
-        user: UserCreator(
-          id: 'u1',
-          username: 'username',
-          avatarUrl: 'avatar/url',
-          themeMode: UserCreatorThemeMode.dark,
-          themePrimaryColor: UserCreatorThemePrimaryColor.brown,
-        ).createEntity(),
-      );
-    },
-    act: (cubit) async => await cubit.initialize(),
-    expect: () => [
-      const ProfileState(
-        status: ProfileStateStatus.completed,
+  group(
+    'initialize, ',
+    () {
+      const String loggedUserId = 'u1';
+      final User loggedUser = UserCreator(
+        id: 'u1',
         username: 'username',
         avatarUrl: 'avatar/url',
-        themeMode: ThemeMode.dark,
-        themePrimaryColor: ThemePrimaryColor.brown,
-      ),
-    ],
-    verify: (_) {
-      verify(() => authRepository.loggedUserId$).called(1);
-      verify(() => userRepository.getUserById(userId: 'u1')).called(1);
-    },
-  );
+        themeMode: UserCreatorThemeMode.dark,
+        themePrimaryColor: UserCreatorThemePrimaryColor.brown,
+      ).createEntity();
 
-  blocTest(
-    'updateAvatar, '
-    'logged user id is null, '
-    'should do nothing',
-    build: () => createCubit(),
-    setUp: () => authRepository.mockGetLoggedUserId(null),
-    act: (cubit) async => await cubit.updateAvatar('avatar/img/path'),
-    expect: () => [],
-    verify: (_) => verify(() => authRepository.loggedUserId$).called(1),
-  );
+      blocTest(
+        'should emit state with status set as loggedUserDoesNotExist if '
+        'logged user id is null',
+        build: () => createCubit(),
+        setUp: () => authRepository.mockGetLoggedUserId(null),
+        act: (cubit) async => await cubit.initialize(),
+        expect: () => [
+          const ProfileState(
+            status: ProfileStateStatus.loggedUserDoesNotExist,
+          ),
+        ],
+        verify: (_) => verify(() => authRepository.loggedUserId$).called(1),
+      );
 
-  blocTest(
-    'updateAvatar, '
-    'should call method from UserRepository to update user avatar',
-    build: () => createCubit(),
-    setUp: () {
-      authRepository.mockGetLoggedUserId('u1');
-      userRepository.mockUpdateUserAvatar();
-    },
-    act: (cubit) async => await cubit.updateAvatar('avatar/img/path'),
-    expect: () => [
-      const ProfileState(
-        status: ProfileStateStatus.loading,
-      ),
-      const ProfileState(
-        status: ProfileStateStatus.completed,
-      ),
-    ],
-    verify: (_) {
-      verify(() => authRepository.loggedUserId$).called(1);
-      verify(
-        () => userRepository.updateUserAvatar(
-          userId: 'u1',
-          avatarImgPath: 'avatar/img/path',
-        ),
-      ).called(1);
-    },
-  );
-
-  blocTest(
-    'updateUsername, '
-    'new username is empty, '
-    'should emit state with status set to newUsernameIsEmpty',
-    build: () => createCubit(),
-    act: (cubit) async => await cubit.updateUsername(''),
-    expect: () => [
-      const ProfileState(
-        status: ProfileStateStatus.newUsernameIsEmpty,
-      ),
-    ],
-  );
-
-  blocTest(
-    'updateUsername, '
-    'logged user id is null, '
-    'should finish method call',
-    build: () => createCubit(),
-    setUp: () => authRepository.mockGetLoggedUserId(null),
-    act: (cubit) async => await cubit.updateUsername('new username'),
-    expect: () => [],
-    verify: (_) => verify(() => authRepository.loggedUserId$).called(1),
-  );
-
-  blocTest(
-    'updateUsername, '
-    'should call method from UserRepository to update user data with new username',
-    build: () => createCubit(),
-    setUp: () {
-      authRepository.mockGetLoggedUserId('u1');
-      userRepository.mockUpdateUserData();
-    },
-    act: (cubit) async => await cubit.updateUsername('new username'),
-    expect: () => [
-      const ProfileState(
-        status: ProfileStateStatus.loading,
-      ),
-      const ProfileState(
-        status: ProfileStateStatus.usernameUpdated,
-      ),
-    ],
-    verify: (_) {
-      verify(() => authRepository.loggedUserId$).called(1);
-      verify(
-        () => userRepository.updateUserData(
-          userId: 'u1',
-          username: 'new username',
-        ),
-      ).called(1);
-    },
-  );
-
-  blocTest(
-    'updateUsername, '
-    'method from UserRepository to update user data throws UsernameAlreadyTaken '
-    'exception, '
-    'should emit state with status set to newUsernameIsAlreadyTaken',
-    build: () => createCubit(),
-    setUp: () {
-      authRepository.mockGetLoggedUserId('u1');
-      userRepository.mockUpdateUserData(
-        throwable: const UserRepositoryExceptionUsernameAlreadyTaken(),
+      blocTest(
+        'should load and emit logged user data',
+        build: () => createCubit(),
+        setUp: () {
+          authRepository.mockGetLoggedUserId(loggedUserId);
+          userRepository.mockGetUserById(user: loggedUser);
+        },
+        act: (cubit) async => await cubit.initialize(),
+        expect: () => [
+          ProfileState(
+            status: ProfileStateStatus.completed,
+            username: loggedUser.username,
+            avatarUrl: loggedUser.avatarUrl,
+            themeMode: loggedUser.themeMode,
+            themePrimaryColor: loggedUser.themePrimaryColor,
+          ),
+        ],
+        verify: (_) {
+          verify(() => authRepository.loggedUserId$).called(1);
+          verify(
+            () => userRepository.getUserById(userId: loggedUserId),
+          ).called(1);
+        },
       );
     },
-    act: (cubit) async => await cubit.updateUsername('new username'),
-    expect: () => [
-      const ProfileState(
-        status: ProfileStateStatus.loading,
-      ),
-      const ProfileState(
-        status: ProfileStateStatus.newUsernameIsAlreadyTaken,
-      ),
-    ],
-    verify: (_) {
-      verify(() => authRepository.loggedUserId$).called(1);
-      verify(
-        () => userRepository.updateUserData(
-          userId: 'u1',
-          username: 'new username',
-        ),
-      ).called(1);
+  );
+
+  group(
+    'updateAvatar, ',
+    () {
+      const String loggedUserId = 'u1';
+      const String newAvatarImgPath = 'avatar/img/path';
+
+      blocTest(
+        'should do nothing if logged user id is null',
+        build: () => createCubit(),
+        setUp: () => authRepository.mockGetLoggedUserId(null),
+        act: (cubit) async => await cubit.updateAvatar(newAvatarImgPath),
+        expect: () => [],
+        verify: (_) => verify(() => authRepository.loggedUserId$).called(1),
+      );
+
+      blocTest(
+        'should call method from UserRepository to update user avatar',
+        build: () => createCubit(),
+        setUp: () {
+          authRepository.mockGetLoggedUserId(loggedUserId);
+          userRepository.mockUpdateUserAvatar();
+        },
+        act: (cubit) async => await cubit.updateAvatar(newAvatarImgPath),
+        expect: () => [
+          const ProfileState(
+            status: ProfileStateStatus.loading,
+          ),
+          const ProfileState(
+            status: ProfileStateStatus.completed,
+          ),
+        ],
+        verify: (_) {
+          verify(() => authRepository.loggedUserId$).called(1);
+          verify(
+            () => userRepository.updateUserAvatar(
+              userId: loggedUserId,
+              avatarImgPath: newAvatarImgPath,
+            ),
+          ).called(1);
+        },
+      );
+    },
+  );
+
+  group(
+    'updateUsername, ',
+    () {
+      const String loggedUserId = 'u1';
+      const String newUsername = 'new username';
+
+      blocTest(
+        'should emit state with status set to newUsernameIsEmpty if new '
+        'username is empty',
+        build: () => createCubit(),
+        act: (cubit) async => await cubit.updateUsername(''),
+        expect: () => [
+          const ProfileState(
+            status: ProfileStateStatus.newUsernameIsEmpty,
+          ),
+        ],
+      );
+
+      blocTest(
+        'should finish method call if logged user id is null',
+        build: () => createCubit(),
+        setUp: () => authRepository.mockGetLoggedUserId(null),
+        act: (cubit) async => await cubit.updateUsername(newUsername),
+        expect: () => [],
+        verify: (_) => verify(() => authRepository.loggedUserId$).called(1),
+      );
+
+      blocTest(
+        'should call method from UserRepository to update user data with '
+        'new username',
+        build: () => createCubit(),
+        setUp: () {
+          authRepository.mockGetLoggedUserId(loggedUserId);
+          userRepository.mockUpdateUserData();
+        },
+        act: (cubit) async => await cubit.updateUsername(newUsername),
+        expect: () => [
+          const ProfileState(
+            status: ProfileStateStatus.loading,
+          ),
+          const ProfileState(
+            status: ProfileStateStatus.usernameUpdated,
+          ),
+        ],
+        verify: (_) {
+          verify(() => authRepository.loggedUserId$).called(1);
+          verify(
+            () => userRepository.updateUserData(
+              userId: loggedUserId,
+              username: newUsername,
+            ),
+          ).called(1);
+        },
+      );
+
+      blocTest(
+        'should emit state with status set to newUsernameIsAlreadyTaken if '
+        'method from UserRepository to update user data throws '
+        'UsernameAlreadyTaken exception',
+        build: () => createCubit(),
+        setUp: () {
+          authRepository.mockGetLoggedUserId(loggedUserId);
+          userRepository.mockUpdateUserData(
+            throwable: const UserRepositoryExceptionUsernameAlreadyTaken(),
+          );
+        },
+        act: (cubit) async => await cubit.updateUsername(newUsername),
+        expect: () => [
+          const ProfileState(
+            status: ProfileStateStatus.loading,
+          ),
+          const ProfileState(
+            status: ProfileStateStatus.newUsernameIsAlreadyTaken,
+          ),
+        ],
+        verify: (_) {
+          verify(() => authRepository.loggedUserId$).called(1);
+          verify(
+            () => userRepository.updateUserData(
+              userId: loggedUserId,
+              username: newUsername,
+            ),
+          ).called(1);
+        },
+      );
     },
   );
 }
