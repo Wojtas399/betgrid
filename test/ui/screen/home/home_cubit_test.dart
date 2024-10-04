0c6f8d304328050d412738fb55acee99436720a1
Wojtas399 <wojtekp26@gmail.com>
2024-10-04 17:34:45 +0200

    Refactored tests for HomeCubit.

--- test/ui/screen/home/home_cubit_test.dart
+++ test/ui/screen/home/home_cubit_test.dart
@@ -1,3 +1,4 @@
+import 'package:betgrid/model/user.dart';
 import 'package:betgrid/ui/screen/home/cubit/home_cubit.dart';
 import 'package:betgrid/ui/screen/home/cubit/home_state.dart';
 import 'package:bloc_test/bloc_test.dart';
@@ -32,298 +33,248 @@ void main() {
     reset(grandPrixRepository);
   });
 
-  blocTest(
-    'initialize, '
-    'logged user id is null, '
-    'should emit state with loggedUserDoesNotExist status',
-    setUp: () => authRepository.mockGetLoggedUserId(null),
-    build: () => createCubit(),
-    act: (cubit) async => await cubit.initialize(),
-    expect: () => [
-      const HomeState(
-        status: HomeStateStatus.loggedUserDoesNotExist,
-      )
-    ],
-    verify: (_) => verify(() => authRepository.loggedUserId$).called(1),
-  );
-
-  blocTest(
-    'initialize, '
-    'logged user data does not exist, '
-    'should emit state with loggedUserDataNotCompleted status',
-    setUp: () {
-      authRepository.mockGetLoggedUserId('u1');
-      userRepository.mockGetUserById(user: null);
-    },
-    build: () => createCubit(),
-    act: (cubit) async => await cubit.initialize(),
-    expect: () => [
-      const HomeState(
-        status: HomeStateStatus.loggedUserDataNotCompleted,
-      )
-    ],
-    verify: (_) {
-      verify(() => authRepository.loggedUserId$).called(1);
-      verify(() => userRepository.getUserById(userId: 'u1')).called(1);
-    },
-  );
-
-  blocTest(
-    'initialize, '
-    'logged user has bets already, '
-    'should emit state with username, avatarUrl and status set as completed',
-    setUp: () {
-      authRepository.mockGetLoggedUserId('u1');
-      userRepository.mockGetUserById(
-        user: UserCreator(
-          id: 'u1',
-          username: 'username',
-          avatarUrl: 'avatar/url',
-        ).createEntity(),
+  group(
+    'initialize, ',
+    () {
+      const String loggedUserId = 'u1';
+      final User loggedUser = UserCreator(
+        id: loggedUserId,
+        username: 'username',
+        avatarUrl: 'avatar/url',
+      ).createEntity();
+      final HomeState expectedState = HomeState(
+        status: HomeStateStatus.completed,
+        username: loggedUser.username,
+        avatarUrl: loggedUser.avatarUrl,
       );
-      grandPrixBetRepository.mockGetAllGrandPrixBetsForPlayer(
-        grandPrixBets: [
-          GrandPrixBetCreator(id: 'gpb1').createEntity(),
-          GrandPrixBetCreator(id: 'gpb2').createEntity(),
+
+      setUp(() {
+        authRepository.mockGetLoggedUserId(loggedUserId);
+        userRepository.mockGetUserById(user: loggedUser);
+      });
+
+      tearDown(() {
+        verify(() => authRepository.loggedUserId$).called(1);
+      });
+
+      blocTest(
+        'should emit state with loggedUserDoesNotExist status if logged user '
+        'id is null',
+        setUp: () => authRepository.mockGetLoggedUserId(null),
+        build: () => createCubit(),
+        act: (cubit) async => await cubit.initialize(),
+        expect: () => [
+          const HomeState(
+            status: HomeStateStatus.loggedUserDoesNotExist,
+          )
         ],
       );
-    },
-    build: () => createCubit(),
-    act: (cubit) async => await cubit.initialize(),
-    expect: () => [
-      const HomeState(
-        status: HomeStateStatus.completed,
-        username: 'username',
-        avatarUrl: 'avatar/url',
-      )
-    ],
-    verify: (_) {
-      verify(() => authRepository.loggedUserId$).called(1);
-      verify(() => userRepository.getUserById(userId: 'u1')).called(1);
-      verify(
-        () => grandPrixBetRepository.getAllGrandPrixBetsForPlayer(
-          playerId: 'u1',
-        ),
-      ).called(1);
-    },
-  );
 
-  blocTest(
-    'initialize, '
-    'list of logged user bets is null and list of all grand prixes is null, '
-    'should emit state with username, avatarUrl and status set as completed',
-    setUp: () {
-      authRepository.mockGetLoggedUserId('u1');
-      userRepository.mockGetUserById(
-        user: UserCreator(
-          id: 'u1',
-          username: 'username',
-          avatarUrl: 'avatar/url',
-        ).createEntity(),
+      blocTest(
+        'should emit state with loggedUserDataNotCompleted status if logged '
+        'user data does not exist',
+        setUp: () => userRepository.mockGetUserById(user: null),
+        build: () => createCubit(),
+        act: (cubit) async => await cubit.initialize(),
+        expect: () => [
+          const HomeState(
+            status: HomeStateStatus.loggedUserDataNotCompleted,
+          )
+        ],
+        verify: (_) => verify(
+          () => userRepository.getUserById(userId: loggedUserId),
+        ).called(1),
       );
-      grandPrixBetRepository.mockGetAllGrandPrixBetsForPlayer(
-        grandPrixBets: null,
-      );
-      grandPrixRepository.mockGetAllGrandPrixes(null);
-    },
-    build: () => createCubit(),
-    act: (cubit) async => await cubit.initialize(),
-    expect: () => [
-      const HomeState(
-        status: HomeStateStatus.completed,
-        username: 'username',
-        avatarUrl: 'avatar/url',
-      )
-    ],
-    verify: (_) {
-      verify(() => authRepository.loggedUserId$).called(1);
-      verify(() => userRepository.getUserById(userId: 'u1')).called(1);
-      verify(
-        () => grandPrixBetRepository.getAllGrandPrixBetsForPlayer(
-          playerId: 'u1',
-        ),
-      ).called(1);
-      verify(() => grandPrixRepository.getAllGrandPrixes()).called(1);
-    },
-  );
 
-  blocTest(
-    'initialize, '
-    'list of logged user bets is null and list of all grand prixes is empty, '
-    'should emit state with username, avatarUrl and status set as completed',
-    setUp: () {
-      authRepository.mockGetLoggedUserId('u1');
-      userRepository.mockGetUserById(
-        user: UserCreator(
-          id: 'u1',
-          username: 'username',
-          avatarUrl: 'avatar/url',
-        ).createEntity(),
+      blocTest(
+        'should emit state with username, avatarUrl and status set as '
+        'completed if logged user already has bets',
+        setUp: () {
+          grandPrixBetRepository.mockGetAllGrandPrixBetsForPlayer(
+            grandPrixBets: [
+              GrandPrixBetCreator(id: 'gpb1').createEntity(),
+              GrandPrixBetCreator(id: 'gpb2').createEntity(),
+            ],
+          );
+        },
+        build: () => createCubit(),
+        act: (cubit) async => await cubit.initialize(),
+        expect: () => [
+          expectedState,
+        ],
+        verify: (_) {
+          verify(
+            () => userRepository.getUserById(userId: loggedUserId),
+          ).called(1);
+          verify(
+            () => grandPrixBetRepository.getAllGrandPrixBetsForPlayer(
+              playerId: loggedUserId,
+            ),
+          ).called(1);
+        },
       );
-      grandPrixBetRepository.mockGetAllGrandPrixBetsForPlayer(
-        grandPrixBets: null,
-      );
-      grandPrixRepository.mockGetAllGrandPrixes([]);
-    },
-    build: () => createCubit(),
-    act: (cubit) async => await cubit.initialize(),
-    expect: () => [
-      const HomeState(
-        status: HomeStateStatus.completed,
-        username: 'username',
-        avatarUrl: 'avatar/url',
-      )
-    ],
-    verify: (_) {
-      verify(() => authRepository.loggedUserId$).called(1);
-      verify(() => userRepository.getUserById(userId: 'u1')).called(1);
-      verify(
-        () => grandPrixBetRepository.getAllGrandPrixBetsForPlayer(
-          playerId: 'u1',
-        ),
-      ).called(1);
-      verify(() => grandPrixRepository.getAllGrandPrixes()).called(1);
-    },
-  );
 
-  blocTest(
-    'initialize, '
-    'list of logged user bets is empty and list of all grand prixes is null, '
-    'should emit state with username, avatarUrl and status set as completed',
-    setUp: () {
-      authRepository.mockGetLoggedUserId('u1');
-      userRepository.mockGetUserById(
-        user: UserCreator(
-          id: 'u1',
-          username: 'username',
-          avatarUrl: 'avatar/url',
-        ).createEntity(),
+      blocTest(
+        'should emit state with username, avatarUrl and status set as '
+        "completed if list of logged user's bets is null and list of all "
+        'grand prixes is null',
+        setUp: () {
+          grandPrixBetRepository.mockGetAllGrandPrixBetsForPlayer(
+            grandPrixBets: null,
+          );
+          grandPrixRepository.mockGetAllGrandPrixes(null);
+        },
+        build: () => createCubit(),
+        act: (cubit) async => await cubit.initialize(),
+        expect: () => [
+          expectedState,
+        ],
+        verify: (_) {
+          verify(
+            () => userRepository.getUserById(userId: loggedUserId),
+          ).called(1);
+          verify(
+            () => grandPrixBetRepository.getAllGrandPrixBetsForPlayer(
+              playerId: loggedUserId,
+            ),
+          ).called(1);
+          verify(() => grandPrixRepository.getAllGrandPrixes()).called(1);
+        },
       );
-      grandPrixBetRepository.mockGetAllGrandPrixBetsForPlayer(
-        grandPrixBets: [],
-      );
-      grandPrixRepository.mockGetAllGrandPrixes(null);
-    },
-    build: () => createCubit(),
-    act: (cubit) async => await cubit.initialize(),
-    expect: () => [
-      const HomeState(
-        status: HomeStateStatus.completed,
-        username: 'username',
-        avatarUrl: 'avatar/url',
-      )
-    ],
-    verify: (_) {
-      verify(() => authRepository.loggedUserId$).called(1);
-      verify(() => userRepository.getUserById(userId: 'u1')).called(1);
-      verify(
-        () => grandPrixBetRepository.getAllGrandPrixBetsForPlayer(
-          playerId: 'u1',
-        ),
-      ).called(1);
-      verify(() => grandPrixRepository.getAllGrandPrixes()).called(1);
-    },
-  );
 
-  blocTest(
-    'initialize, '
-    'list of logged user bets is empty and list of all grand prixes is empty, '
-    'should emit state with username, avatarUrl and status set as completed',
-    setUp: () {
-      authRepository.mockGetLoggedUserId('u1');
-      userRepository.mockGetUserById(
-        user: UserCreator(
-          id: 'u1',
-          username: 'username',
-          avatarUrl: 'avatar/url',
-        ).createEntity(),
+      blocTest(
+        'should emit state with username, avatarUrl and status set as '
+        "completed if list of logged user's bets is null and list of all "
+        'grand prixes is empty',
+        setUp: () {
+          grandPrixBetRepository.mockGetAllGrandPrixBetsForPlayer(
+            grandPrixBets: null,
+          );
+          grandPrixRepository.mockGetAllGrandPrixes([]);
+        },
+        build: () => createCubit(),
+        act: (cubit) async => await cubit.initialize(),
+        expect: () => [
+          expectedState,
+        ],
+        verify: (_) {
+          verify(
+            () => userRepository.getUserById(userId: loggedUserId),
+          ).called(1);
+          verify(
+            () => grandPrixBetRepository.getAllGrandPrixBetsForPlayer(
+              playerId: loggedUserId,
+            ),
+          ).called(1);
+          verify(() => grandPrixRepository.getAllGrandPrixes()).called(1);
+        },
       );
-      grandPrixBetRepository.mockGetAllGrandPrixBetsForPlayer(
-        grandPrixBets: [],
-      );
-      grandPrixRepository.mockGetAllGrandPrixes([]);
-    },
-    build: () => createCubit(),
-    act: (cubit) async => await cubit.initialize(),
-    expect: () => [
-      const HomeState(
-        status: HomeStateStatus.completed,
-        username: 'username',
-        avatarUrl: 'avatar/url',
-      )
-    ],
-    verify: (_) {
-      verify(() => authRepository.loggedUserId$).called(1);
-      verify(() => userRepository.getUserById(userId: 'u1')).called(1);
-      verify(
-        () => grandPrixBetRepository.getAllGrandPrixBetsForPlayer(
-          playerId: 'u1',
-        ),
-      ).called(1);
-      verify(() => grandPrixRepository.getAllGrandPrixes()).called(1);
-    },
-  );
 
-  blocTest(
-    'initialize, '
-    'should create default bets for each grand prix and should emit state with '
-    'username, avatarUrl and status set as completed',
-    setUp: () {
-      authRepository.mockGetLoggedUserId('u1');
-      userRepository.mockGetUserById(
-        user: UserCreator(
-          id: 'u1',
-          username: 'username',
-          avatarUrl: 'avatar/url',
-        ).createEntity(),
+      blocTest(
+        'should emit state with username, avatarUrl and status set as '
+        "completed if list of logged user's bets is empty and list of all "
+        'grand prixes is null',
+        setUp: () {
+          grandPrixBetRepository.mockGetAllGrandPrixBetsForPlayer(
+            grandPrixBets: [],
+          );
+          grandPrixRepository.mockGetAllGrandPrixes(null);
+        },
+        build: () => createCubit(),
+        act: (cubit) async => await cubit.initialize(),
+        expect: () => [
+          expectedState,
+        ],
+        verify: (_) {
+          verify(
+            () => userRepository.getUserById(userId: loggedUserId),
+          ).called(1);
+          verify(
+            () => grandPrixBetRepository.getAllGrandPrixBetsForPlayer(
+              playerId: loggedUserId,
+            ),
+          ).called(1);
+          verify(() => grandPrixRepository.getAllGrandPrixes()).called(1);
+        },
       );
-      grandPrixBetRepository.mockGetAllGrandPrixBetsForPlayer(
-        grandPrixBets: null,
+
+      blocTest(
+        'should emit state with username, avatarUrl and status set as '
+        "completed if list of logged user's bets is empty and list of all "
+        'grand prixes is empty',
+        setUp: () {
+          grandPrixBetRepository.mockGetAllGrandPrixBetsForPlayer(
+            grandPrixBets: [],
+          );
+          grandPrixRepository.mockGetAllGrandPrixes([]);
+        },
+        build: () => createCubit(),
+        act: (cubit) async => await cubit.initialize(),
+        expect: () => [
+          expectedState,
+        ],
+        verify: (_) {
+          verify(
+            () => userRepository.getUserById(userId: loggedUserId),
+          ).called(1);
+          verify(
+            () => grandPrixBetRepository.getAllGrandPrixBetsForPlayer(
+              playerId: loggedUserId,
+            ),
+          ).called(1);
+          verify(() => grandPrixRepository.getAllGrandPrixes()).called(1);
+        },
       );
-      grandPrixRepository.mockGetAllGrandPrixes([
-        GrandPrixCreator(id: 'gp1').createEntity(),
-        GrandPrixCreator(id: 'gp2').createEntity(),
-        GrandPrixCreator(id: 'gp3').createEntity(),
-      ]);
-      grandPrixBetRepository.mockAddGrandPrixBetsForPlayer();
-    },
-    build: () => createCubit(),
-    act: (cubit) async => await cubit.initialize(),
-    expect: () => [
-      const HomeState(
-        status: HomeStateStatus.completed,
-        username: 'username',
-        avatarUrl: 'avatar/url',
-      )
-    ],
-    verify: (_) {
-      verify(() => authRepository.loggedUserId$).called(1);
-      verify(() => userRepository.getUserById(userId: 'u1')).called(1);
-      verify(
-        () => grandPrixBetRepository.getAllGrandPrixBetsForPlayer(
-          playerId: 'u1',
-        ),
-      ).called(1);
-      verify(() => grandPrixRepository.getAllGrandPrixes()).called(1);
-      verify(
-        () => grandPrixBetRepository.addGrandPrixBetsForPlayer(
-          playerId: 'u1',
-          grandPrixBets: [
-            GrandPrixBetCreator(
-              playerId: 'u1',
-              grandPrixId: 'gp1',
-            ).createEntity(),
-            GrandPrixBetCreator(
-              playerId: 'u1',
-              grandPrixId: 'gp2',
-            ).createEntity(),
-            GrandPrixBetCreator(
-              playerId: 'u1',
-              grandPrixId: 'gp3',
-            ).createEntity(),
-          ],
-        ),
-      ).called(1);
+
+      blocTest(
+        'should create default bets for each grand prix and should emit state '
+        'with username, avatarUrl and status set as completed',
+        setUp: () {
+          grandPrixBetRepository.mockGetAllGrandPrixBetsForPlayer(
+            grandPrixBets: null,
+          );
+          grandPrixRepository.mockGetAllGrandPrixes([
+            GrandPrixCreator(id: 'gp1').createEntity(),
+            GrandPrixCreator(id: 'gp2').createEntity(),
+            GrandPrixCreator(id: 'gp3').createEntity(),
+          ]);
+          grandPrixBetRepository.mockAddGrandPrixBetsForPlayer();
+        },
+        build: () => createCubit(),
+        act: (cubit) async => await cubit.initialize(),
+        expect: () => [
+          expectedState,
+        ],
+        verify: (_) {
+          verify(() => userRepository.getUserById(userId: loggedUserId))
+              .called(1);
+          verify(
+            () => grandPrixBetRepository.getAllGrandPrixBetsForPlayer(
+              playerId: loggedUserId,
+            ),
+          ).called(1);
+          verify(() => grandPrixRepository.getAllGrandPrixes()).called(1);
+          verify(
+            () => grandPrixBetRepository.addGrandPrixBetsForPlayer(
+              playerId: loggedUserId,
+              grandPrixBets: [
+                GrandPrixBetCreator(
+                  playerId: loggedUserId,
+                  grandPrixId: 'gp1',
+                ).createEntity(),
+                GrandPrixBetCreator(
+                  playerId: loggedUserId,
+                  grandPrixId: 'gp2',
+                ).createEntity(),
+                GrandPrixBetCreator(
+                  playerId: loggedUserId,
+                  grandPrixId: 'gp3',
+                ).createEntity(),
+              ],
+            ),
+          ).called(1);
+        },
+      );
     },
   );
 }
