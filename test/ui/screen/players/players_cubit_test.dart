2e28c0a6119bc1ef3fce84ee0ce8bf567c806b27
Wojtas399 <wojtekp26@gmail.com>
2024-10-04 17:38:15 +0200

    Refactored tests for PlayersCubit.

--- test/ui/screen/players/players_cubit_test.dart
+++ test/ui/screen/players/players_cubit_test.dart
@@ -1,3 +1,4 @@
+import 'package:betgrid/model/player.dart';
 import 'package:betgrid/ui/screen/players/cubit/players_cubit.dart';
 import 'package:betgrid/ui/screen/players/cubit/players_state.dart';
 import 'package:bloc_test/bloc_test.dart';
@@ -26,62 +27,70 @@ void main() {
     reset(getPlayerPointsUseCase);
   });
 
-  blocTest(
-    'initialize, '
-    'logged user id is null, '
-    'should emit state with status set to loggedUserDoesNotExist',
-    build: () => createCubit(),
-    setUp: () => authRepository.mockGetLoggedUserId(null),
-    act: (cubit) async => await cubit.initialize(),
-    expect: () => [
-      const PlayersState(
-        status: PlayersStateStatus.loggedUserDoesNotExist,
-      ),
-    ],
-    verify: (_) => verify(() => authRepository.loggedUserId$).called(1),
-  );
+  group(
+    'initialize, ',
+    () {
+      const String loggedUserId = 'u1';
+      final List<Player> players = [
+        PlayerCreator(id: loggedUserId).createEntity(),
+        PlayerCreator(id: 'u2').createEntity(),
+        PlayerCreator(id: 'u3').createEntity(),
+      ];
 
-  blocTest(
-    'initialize, '
-    'should load and emit other players with their points',
-    build: () => createCubit(),
-    setUp: () {
-      authRepository.mockGetLoggedUserId('u1');
-      playerRepository.mockGetAllPlayers(
-        players: [
-          PlayerCreator(id: 'u1').createEntity(),
-          PlayerCreator(id: 'u2').createEntity(),
-          PlayerCreator(id: 'u3').createEntity(),
+      blocTest(
+        'should emit state with status set to loggedUserDoesNotExist if logged '
+        'user id is null',
+        build: () => createCubit(),
+        setUp: () => authRepository.mockGetLoggedUserId(null),
+        act: (cubit) async => await cubit.initialize(),
+        expect: () => [
+          const PlayersState(
+            status: PlayersStateStatus.loggedUserDoesNotExist,
+          ),
         ],
+        verify: (_) => verify(() => authRepository.loggedUserId$).called(1),
       );
-      when(
-        () => getPlayerPointsUseCase.call(playerId: 'u2'),
-      ).thenAnswer((_) => Stream.value(12.5));
-      when(
-        () => getPlayerPointsUseCase.call(playerId: 'u3'),
-      ).thenAnswer((_) => Stream.value(22.2));
-    },
-    act: (cubit) async => await cubit.initialize(),
-    expect: () => [
-      PlayersState(
-        status: PlayersStateStatus.completed,
-        playersWithTheirPoints: [
-          PlayerWithPoints(
-            player: PlayerCreator(id: 'u2').createEntity(),
-            totalPoints: 12.5,
-          ),
-          PlayerWithPoints(
-            player: PlayerCreator(id: 'u3').createEntity(),
-            totalPoints: 22.2,
+
+      blocTest(
+        'should load and emit other players with their points',
+        build: () => createCubit(),
+        setUp: () {
+          authRepository.mockGetLoggedUserId(loggedUserId);
+          playerRepository.mockGetAllPlayers(players: players);
+          when(
+            () => getPlayerPointsUseCase.call(playerId: 'u2'),
+          ).thenAnswer((_) => Stream.value(12.5));
+          when(
+            () => getPlayerPointsUseCase.call(playerId: 'u3'),
+          ).thenAnswer((_) => Stream.value(22.2));
+        },
+        act: (cubit) async => await cubit.initialize(),
+        expect: () => [
+          PlayersState(
+            status: PlayersStateStatus.completed,
+            playersWithTheirPoints: [
+              PlayerWithPoints(
+                player: players[1],
+                totalPoints: 12.5,
+              ),
+              PlayerWithPoints(
+                player: players.last,
+                totalPoints: 22.2,
+              ),
+            ],
           ),
         ],
-      ),
-    ],
-    verify: (_) {
-      verify(() => authRepository.loggedUserId$).called(1);
-      verify(() => playerRepository.getAllPlayers()).called(1);
-      verify(() => getPlayerPointsUseCase.call(playerId: 'u2')).called(1);
-      verify(() => getPlayerPointsUseCase.call(playerId: 'u3')).called(1);
+        verify: (_) {
+          verify(() => authRepository.loggedUserId$).called(1);
+          verify(() => playerRepository.getAllPlayers()).called(1);
+          verify(
+            () => getPlayerPointsUseCase.call(playerId: 'u2'),
+          ).called(1);
+          verify(
+            () => getPlayerPointsUseCase.call(playerId: 'u3'),
+          ).called(1);
+        },
+      );
     },
   );
 }
