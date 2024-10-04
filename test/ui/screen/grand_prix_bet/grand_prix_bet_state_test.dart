c9dc7be4edfe4224fbeee20c5fb9b5f9e8c49375
Wojtas399 <wojtekp26@gmail.com>
2024-10-04 16:51:04 +0200

    Refactored tests for GrandPrixBetState.

--- test/ui/screen/grand_prix_bet/grand_prix_bet_state_test.dart
+++ test/ui/screen/grand_prix_bet/grand_prix_bet_state_test.dart
@@ -5,69 +5,68 @@ import 'package:flutter_test/flutter_test.dart';
 import '../../../creator/driver_creator.dart';
 
 void main() {
-  test(
-    'getDriverById, '
-    'list of all drivers is null, '
-    'should return null',
+  group(
+    'getDriveById, ',
     () {
-      const state = GrandPrixBetState();
+      test(
+        'should return null if list of all drivers is null',
+        () {
+          const state = GrandPrixBetState();
 
-      final Driver? driver = state.getDriverById('d1');
+          final Driver? driver = state.getDriverById('d1');
 
-      expect(driver, null);
-    },
-  );
-
-  test(
-    'getDriverById, '
-    'list of all drivers is empty, '
-    'should return null',
-    () {
-      const state = GrandPrixBetState(
-        allDrivers: [],
+          expect(driver, null);
+        },
       );
 
-      final Driver? driver = state.getDriverById('d1');
+      test(
+        'should return null if list of all drivers is empty',
+        () {
+          const state = GrandPrixBetState(
+            allDrivers: [],
+          );
 
-      expect(driver, null);
-    },
-  );
+          final Driver? driver = state.getDriverById('d1');
 
-  test(
-    'getDriverById, '
-    'driver does not exist in the list of all drivers, '
-    'should return null',
-    () {
-      final state = GrandPrixBetState(
-        allDrivers: [
-          DriverCreator(id: 'd2').createEntity(),
-          DriverCreator(id: 'd3').createEntity(),
-          DriverCreator(id: 'd4').createEntity(),
-        ],
+          expect(driver, null);
+        },
       );
 
-      final Driver? driver = state.getDriverById('d1');
+      test(
+        'should return null if driver does not exist in the list of all '
+        'drivers',
+        () {
+          final state = GrandPrixBetState(
+            allDrivers: [
+              DriverCreator(id: 'd2').createEntity(),
+              DriverCreator(id: 'd3').createEntity(),
+              DriverCreator(id: 'd4').createEntity(),
+            ],
+          );
 
-      expect(driver, null);
-    },
-  );
+          final Driver? driver = state.getDriverById('d1');
 
-  test(
-    'getDriverById, '
-    'should return matching driver from the list of all drivers',
-    () {
-      final state = GrandPrixBetState(
-        allDrivers: [
-          DriverCreator(id: 'd1').createEntity(),
-          DriverCreator(id: 'd2').createEntity(),
-          DriverCreator(id: 'd3').createEntity(),
-          DriverCreator(id: 'd4').createEntity(),
-        ],
+          expect(driver, null);
+        },
       );
 
-      final Driver? driver = state.getDriverById('d1');
+      test(
+        'should return matching driver from the list of all drivers',
+        () {
+          final state = GrandPrixBetState(
+            allDrivers: [
+              DriverCreator(id: 'd1').createEntity(),
+              DriverCreator(id: 'd2').createEntity(),
+              DriverCreator(id: 'd3').createEntity(),
+              DriverCreator(id: 'd4').createEntity(),
+            ],
+          );
 
-      expect(driver, DriverCreator(id: 'd1').createEntity());
+          final Driver? driver = state.getDriverById('d1');
+
+          expect(driver, DriverCreator(id: 'd1').createEntity());
+        },
+      );
     },
   );
 }
