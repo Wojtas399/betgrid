1df0ec6d5c68795bbf408b2763624eb81f413945
Wojtas399 <wojtekp26@gmail.com>
2024-10-04 16:49:26 +0200

    Refactored tests for GrandPrixBetCubit.

--- test/ui/screen/grand_prix_bet/grand_prix_bet_cubit_test.dart
+++ test/ui/screen/grand_prix_bet/grand_prix_bet_cubit_test.dart
@@ -1,3 +1,9 @@
+import 'package:betgrid/model/driver.dart';
+import 'package:betgrid/model/grand_prix.dart';
+import 'package:betgrid/model/grand_prix_bet.dart';
+import 'package:betgrid/model/grand_prix_bet_points.dart';
+import 'package:betgrid/model/grand_prix_results.dart';
+import 'package:betgrid/model/player.dart';
 import 'package:betgrid/ui/screen/grand_prix_bet/cubit/grand_prix_bet_cubit.dart';
 import 'package:betgrid/ui/screen/grand_prix_bet/cubit/grand_prix_bet_state.dart';
 import 'package:bloc_test/bloc_test.dart';
@@ -47,171 +53,119 @@ void main() {
     reset(driverRepository);
   });
 
-  blocTest(
-    'initialize, '
-    'player is logged user, '
-    'should load player username, grand prix name, results, bets and bets points, '
-    'allDrivers and should set isPlayerIdSameAsLoggedUserId param as true',
-    setUp: () {
-      authRepository.mockGetLoggedUserId('p1');
-      playerRepository.mockGetPlayerById(
-        player: PlayerCreator(username: 'username').createEntity(),
-      );
-      grandPrixRepository.mockGetGrandPrixById(
-        GrandPrixCreator(name: 'grand prix').createEntity(),
-      );
-      grandPrixBetRepository.mockGetGrandPrixBetForPlayerAndGrandPrix(
-        grandPrixBet: GrandPrixBetCreator(id: 'gpb1').createEntity(),
-      );
-      grandPrixResultsRepository.mockGetGrandPrixResultsForGrandPrix(
-        results: GrandPrixResultsCreator(id: 'gpr1').createEntity(),
-      );
-      grandPrixBetPointsRepository
-          .mockGetGrandPrixBetPointsForPlayerAndGrandPrix(
-        grandPrixBetPoints:
-            GrandPrixBetPointsCreator(id: 'gpbp1').createEntity(),
-      );
-      driverRepository.mockGetAllDrivers(
-        allDrivers: [
-          DriverCreator(id: 'd1').createEntity(),
-          DriverCreator(id: 'd2').createEntity(),
-          DriverCreator(id: 'd3').createEntity(),
-        ],
-      );
-    },
-    build: () => createCubit(),
-    act: (cubit) => cubit.initialize(
-      playerId: 'p1',
-      grandPrixId: 'gp1',
-    ),
-    expect: () => [
-      GrandPrixBetState(
+  group(
+    'initialize, ',
+    () {
+      const String playerId = 'p1';
+      const String grandPrixId = 'gp1';
+      final Player player = PlayerCreator(username: 'username').createEntity();
+      final GrandPrix grandPrix = GrandPrixCreator(
+        name: 'grand prix',
+      ).createEntity();
+      final GrandPrixBet grandPrixBet = GrandPrixBetCreator(
+        id: 'gpb1',
+      ).createEntity();
+      final GrandPrixResults grandPrixResults = GrandPrixResultsCreator(
+        id: 'gpr1',
+      ).createEntity();
+      final GrandPrixBetPoints grandPrixBetPoints = GrandPrixBetPointsCreator(
+        id: 'gpbp1',
+      ).createEntity();
+      final List<Driver> allDrivers = [
+        DriverCreator(id: 'd1').createEntity(),
+        DriverCreator(id: 'd2').createEntity(),
+        DriverCreator(id: 'd3').createEntity(),
+      ];
+      final GrandPrixBetState expectedState = GrandPrixBetState(
         status: GrandPrixBetStateStatus.completed,
-        playerUsername: 'username',
-        grandPrixName: 'grand prix',
-        isPlayerIdSameAsLoggedUserId: true,
-        grandPrixBet: GrandPrixBetCreator(id: 'gpb1').createEntity(),
-        grandPrixResults: GrandPrixResultsCreator(id: 'gpr1').createEntity(),
-        grandPrixBetPoints:
-            GrandPrixBetPointsCreator(id: 'gpbp1').createEntity(),
-        allDrivers: [
-          DriverCreator(id: 'd1').createEntity(),
-          DriverCreator(id: 'd2').createEntity(),
-          DriverCreator(id: 'd3').createEntity(),
-        ],
-      ),
-    ],
-    verify: (_) {
-      verify(() => authRepository.loggedUserId$).called(1);
-      verify(() => playerRepository.getPlayerById(playerId: 'p1')).called(1);
-      verify(
-        () => grandPrixRepository.getGrandPrixById(
-          grandPrixId: 'gp1',
-        ),
-      ).called(1);
-      verify(
-        () => grandPrixBetRepository.getGrandPrixBetForPlayerAndGrandPrix(
-          playerId: 'p1',
-          grandPrixId: 'gp1',
-        ),
-      ).called(1);
-      verify(
-        () => grandPrixResultsRepository.getGrandPrixResultsForGrandPrix(
-          grandPrixId: 'gp1',
-        ),
-      ).called(1);
-      verify(
-        () => grandPrixBetPointsRepository
-            .getGrandPrixBetPointsForPlayerAndGrandPrix(
-          playerId: 'p1',
-          grandPrixId: 'gp1',
-        ),
-      ).called(1);
-      verify(driverRepository.getAllDrivers).called(1);
-    },
-  );
+        playerUsername: player.username,
+        grandPrixName: grandPrix.name,
+        grandPrixBet: grandPrixBet,
+        grandPrixResults: grandPrixResults,
+        grandPrixBetPoints: grandPrixBetPoints,
+        allDrivers: allDrivers,
+      );
 
-  blocTest(
-    'initialize, '
-    'player is not logged user, '
-    'should load player username, grand prix name, results, bets and bets points, '
-    'allDrivers and should set isPlayerIdSameAsLoggedUserId param as false',
-    setUp: () {
-      authRepository.mockGetLoggedUserId('u1');
-      playerRepository.mockGetPlayerById(
-        player: PlayerCreator(username: 'username').createEntity(),
-      );
-      grandPrixRepository.mockGetGrandPrixById(
-        GrandPrixCreator(name: 'grand prix').createEntity(),
-      );
-      grandPrixBetRepository.mockGetGrandPrixBetForPlayerAndGrandPrix(
-        grandPrixBet: GrandPrixBetCreator(id: 'gpb1').createEntity(),
-      );
-      grandPrixResultsRepository.mockGetGrandPrixResultsForGrandPrix(
-        results: GrandPrixResultsCreator(id: 'gpr1').createEntity(),
-      );
-      grandPrixBetPointsRepository
-          .mockGetGrandPrixBetPointsForPlayerAndGrandPrix(
-        grandPrixBetPoints:
-            GrandPrixBetPointsCreator(id: 'gpbp1').createEntity(),
-      );
-      driverRepository.mockGetAllDrivers(
-        allDrivers: [
-          DriverCreator(id: 'd1').createEntity(),
-          DriverCreator(id: 'd2').createEntity(),
-          DriverCreator(id: 'd3').createEntity(),
+      setUp(() {
+        playerRepository.mockGetPlayerById(player: player);
+        grandPrixRepository.mockGetGrandPrixById(grandPrix);
+        grandPrixBetRepository.mockGetGrandPrixBetForPlayerAndGrandPrix(
+          grandPrixBet: grandPrixBet,
+        );
+        grandPrixResultsRepository.mockGetGrandPrixResultsForGrandPrix(
+          results: grandPrixResults,
+        );
+        grandPrixBetPointsRepository
+            .mockGetGrandPrixBetPointsForPlayerAndGrandPrix(
+          grandPrixBetPoints: grandPrixBetPoints,
+        );
+        driverRepository.mockGetAllDrivers(allDrivers: allDrivers);
+      });
+
+      tearDown(() {
+        verify(() => authRepository.loggedUserId$).called(1);
+        verify(
+          () => playerRepository.getPlayerById(playerId: playerId),
+        ).called(1);
+        verify(
+          () => grandPrixRepository.getGrandPrixById(
+            grandPrixId: grandPrixId,
+          ),
+        ).called(1);
+        verify(
+          () => grandPrixBetRepository.getGrandPrixBetForPlayerAndGrandPrix(
+            playerId: playerId,
+            grandPrixId: grandPrixId,
+          ),
+        ).called(1);
+        verify(
+          () => grandPrixResultsRepository.getGrandPrixResultsForGrandPrix(
+            grandPrixId: grandPrixId,
+          ),
+        ).called(1);
+        verify(
+          () => grandPrixBetPointsRepository
+              .getGrandPrixBetPointsForPlayerAndGrandPrix(
+            playerId: playerId,
+            grandPrixId: grandPrixId,
+          ),
+        ).called(1);
+        verify(driverRepository.getAllDrivers).called(1);
+      });
+
+      blocTest(
+        "should load player's username, grand prix name, results, bet, bet "
+        'points, allDrivers and should set isPlayerIdSameAsLoggedUserId param '
+        'as true if player is logged user',
+        setUp: () => authRepository.mockGetLoggedUserId(playerId),
+        build: () => createCubit(),
+        act: (cubit) => cubit.initialize(
+          playerId: playerId,
+          grandPrixId: grandPrixId,
+        ),
+        expect: () => [
+          expectedState.copyWith(
+            isPlayerIdSameAsLoggedUserId: true,
+          ),
         ],
       );
-    },
-    build: () => createCubit(),
-    act: (cubit) => cubit.initialize(
-      playerId: 'p1',
-      grandPrixId: 'gp1',
-    ),
-    expect: () => [
-      GrandPrixBetState(
-        status: GrandPrixBetStateStatus.completed,
-        playerUsername: 'username',
-        grandPrixName: 'grand prix',
-        isPlayerIdSameAsLoggedUserId: false,
-        grandPrixBet: GrandPrixBetCreator(id: 'gpb1').createEntity(),
-        grandPrixResults: GrandPrixResultsCreator(id: 'gpr1').createEntity(),
-        grandPrixBetPoints:
-            GrandPrixBetPointsCreator(id: 'gpbp1').createEntity(),
-        allDrivers: [
-          DriverCreator(id: 'd1').createEntity(),
-          DriverCreator(id: 'd2').createEntity(),
-          DriverCreator(id: 'd3').createEntity(),
+
+      blocTest(
+        "should load player's username, grand prix name, results, bet, bet "
+        'points, allDrivers and should set isPlayerIdSameAsLoggedUserId param '
+        'as false if player is not logged user',
+        setUp: () => authRepository.mockGetLoggedUserId('u1'),
+        build: () => createCubit(),
+        act: (cubit) => cubit.initialize(
+          playerId: playerId,
+          grandPrixId: grandPrixId,
+        ),
+        expect: () => [
+          expectedState.copyWith(
+            isPlayerIdSameAsLoggedUserId: false,
+          ),
         ],
-      ),
-    ],
-    verify: (_) {
-      verify(() => authRepository.loggedUserId$).called(1);
-      verify(() => playerRepository.getPlayerById(playerId: 'p1')).called(1);
-      verify(
-        () => grandPrixRepository.getGrandPrixById(
-          grandPrixId: 'gp1',
-        ),
-      ).called(1);
-      verify(
-        () => grandPrixBetRepository.getGrandPrixBetForPlayerAndGrandPrix(
-          playerId: 'p1',
-          grandPrixId: 'gp1',
-        ),
-      ).called(1);
-      verify(
-        () => grandPrixResultsRepository.getGrandPrixResultsForGrandPrix(
-          grandPrixId: 'gp1',
-        ),
-      ).called(1);
-      verify(
-        () => grandPrixBetPointsRepository
-            .getGrandPrixBetPointsForPlayerAndGrandPrix(
-          playerId: 'p1',
-          grandPrixId: 'gp1',
-        ),
-      ).called(1);
-      verify(driverRepository.getAllDrivers).called(1);
+      );
     },
   );
 }
