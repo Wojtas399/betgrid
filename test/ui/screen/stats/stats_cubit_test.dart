c84708c3f7f76da28f97fca179ce827e34f725da
Wojtas399 <wojtekp26@gmail.com>
2024-10-05 16:29:31 +0200

    Refactored tests for StatsCubit.

--- test/ui/screen/stats/stats_cubit_test.dart
+++ test/ui/screen/stats/stats_cubit_test.dart
@@ -1,3 +1,4 @@
+import 'package:betgrid/model/driver.dart';
 import 'package:betgrid/ui/screen/stats/cubit/stats_cubit.dart';
 import 'package:betgrid/ui/screen/stats/cubit/stats_state.dart';
 import 'package:betgrid/ui/screen/stats/stats_model/players_podium.dart';
@@ -34,27 +35,20 @@ void main() {
     reset(createPointsForDriverStats);
   });
 
-  blocTest(
-    'initialize, '
-    'should emit state with data of players podium chart and points history '
-    'chart and list of all drivers',
-    build: () => createCubit(),
-    setUp: () {
-      createPlayersPodiumStats.mock(
-        playersPodium: PlayersPodium(
-          p1Player: PlayersPodiumPlayer(
-            player: PlayerCreator(id: 'p3').createEntity(),
-            points: 33,
-          ),
+  group(
+    'initialize, ',
+    () {
+      final PlayersPodium playersPodium = PlayersPodium(
+        p1Player: PlayersPodiumPlayer(
+          player: PlayerCreator(id: 'p3').createEntity(),
+          points: 33,
         ),
       );
-      createPointsHistoryStats.mock(
-        pointsHistory: const PointsHistory(
-          players: [],
-          grandPrixes: [],
-        ),
+      const PointsHistory pointsHistory = PointsHistory(
+        players: [],
+        grandPrixes: [],
       );
-      driverRepository.mockGetAllDrivers(allDrivers: [
+      final List<Driver> allDrivers = [
         DriverCreator(
           id: 'd1',
           team: DriverCreatorTeam.ferrari,
@@ -67,75 +61,73 @@ void main() {
           id: 'd3',
           team: DriverCreatorTeam.alpine,
         ).createEntity(),
-      ]);
-    },
-    act: (cubit) async => await cubit.initialize(),
-    expect: () => [
-      StatsState(
-        status: StatsStateStatus.completed,
-        playersPodium: PlayersPodium(
-          p1Player: PlayersPodiumPlayer(
-            player: PlayerCreator(id: 'p3').createEntity(),
-            points: 33,
+      ];
+
+      blocTest(
+        'should emit state with data of players podium chart and points '
+        'history chart and list of all drivers sorted by team',
+        build: () => createCubit(),
+        setUp: () {
+          createPlayersPodiumStats.mock(
+            playersPodium: playersPodium,
+          );
+          createPointsHistoryStats.mock(
+            pointsHistory: pointsHistory,
+          );
+          driverRepository.mockGetAllDrivers(allDrivers: allDrivers);
+        },
+        act: (cubit) async => await cubit.initialize(),
+        expect: () => [
+          StatsState(
+            status: StatsStateStatus.completed,
+            playersPodium: playersPodium,
+            pointsHistory: pointsHistory,
+            pointsByDriver: [],
+            allDrivers: [
+              allDrivers.last,
+              allDrivers.first,
+              allDrivers[1],
+            ],
           ),
-        ),
-        pointsHistory: const PointsHistory(
-          players: [],
-          grandPrixes: [],
-        ),
-        pointsByDriver: [],
-        allDrivers: [
-          DriverCreator(
-            id: 'd3',
-            team: DriverCreatorTeam.alpine,
-          ).createEntity(),
-          DriverCreator(
-            id: 'd1',
-            team: DriverCreatorTeam.ferrari,
-          ).createEntity(),
-          DriverCreator(
-            id: 'd2',
-            team: DriverCreatorTeam.redBullRacing,
-          ).createEntity(),
         ],
-      ),
-    ],
-    verify: (_) {
-      verify(createPlayersPodiumStats.call).called(1);
-      verify(createPointsHistoryStats.call).called(1);
-      verify(() => driverRepository.getAllDrivers()).called(1);
+        verify: (_) {
+          verify(createPlayersPodiumStats.call).called(1);
+          verify(createPointsHistoryStats.call).called(1);
+          verify(() => driverRepository.getAllDrivers()).called(1);
+        },
+      );
     },
   );
 
-  blocTest(
-    'onDriverChanged, '
-    'should emit state with points for driver chart data',
-    build: () => createCubit(),
-    setUp: () => createPointsForDriverStats.mock(
-      playersPointsForDriver: [
-        PointsByDriverPlayerPoints(
-          player: PlayerCreator(id: 'p1').createEntity(),
-          points: 22.22,
-        ),
-      ],
-    ),
-    act: (cubit) async => await cubit.onDriverChanged('d1'),
-    expect: () => [
-      const StatsState(
-        status: StatsStateStatus.pointsForDriverLoading,
-      ),
-      StatsState(
-        status: StatsStateStatus.completed,
-        pointsByDriver: [
+  group(
+    'onDriverChanged, ',
+    () {
+      final PointsByDriverPlayerPoints playerPointsForDriver =
           PointsByDriverPlayerPoints(
-            player: PlayerCreator(id: 'p1').createEntity(),
-            points: 22.22,
+        player: PlayerCreator(id: 'p1').createEntity(),
+        points: 22.22,
+      );
+
+      blocTest(
+        'should emit state with points for driver chart data',
+        build: () => createCubit(),
+        setUp: () => createPointsForDriverStats.mock(
+          playersPointsForDriver: [playerPointsForDriver],
+        ),
+        act: (cubit) async => await cubit.onDriverChanged('d1'),
+        expect: () => [
+          const StatsState(
+            status: StatsStateStatus.pointsForDriverLoading,
+          ),
+          StatsState(
+            status: StatsStateStatus.completed,
+            pointsByDriver: [playerPointsForDriver],
           ),
         ],
-      ),
-    ],
-    verify: (_) => verify(
-      () => createPointsForDriverStats.call(driverId: 'd1'),
-    ).called(1),
+        verify: (_) => verify(
+          () => createPointsForDriverStats.call(driverId: 'd1'),
+        ).called(1),
+      );
+    },
   );
 }
