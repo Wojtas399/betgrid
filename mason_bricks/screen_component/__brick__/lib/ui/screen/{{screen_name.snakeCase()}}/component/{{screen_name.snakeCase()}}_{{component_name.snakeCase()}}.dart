33eca4b8a4c42b2bd6378e5b840cec103cdd652d
Wojtas399 <wojtekp26@gmail.com>
2024-10-16 19:36:39 +0200

    Added mason bricks.

---
+++ mason_bricks/screen_component/__brick__/lib/ui/screen/{{screen_name.snakeCase()}}/component/{{screen_name.snakeCase()}}_{{component_name.snakeCase()}}.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class {{screen_name.pascalCase()}}{{component_name.pascalCase()}} extends StatelessWidget {
+  const {{screen_name.pascalCase()}}{{component_name.pascalCase()}}({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
