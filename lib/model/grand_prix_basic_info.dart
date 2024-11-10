c4d82c487001f17a88c64d4bb237ff58d1b6e89f
Wojtas399 <wojtekp26@gmail.com>
2024-11-10 14:39:38 +0100

    GrandPrixBasicInfo entity.

---
+++ lib/model/grand_prix_basic_info.dart
@@ -0,0 +1,19 @@
+import 'entity.dart';
+
+class GrandPrixBasicInfo extends Entity {
+  final String name;
+  final String countryAlpha2Code;
+
+  const GrandPrixBasicInfo({
+    required super.id,
+    required this.name,
+    required this.countryAlpha2Code,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        name,
+        countryAlpha2Code,
+      ];
+}
