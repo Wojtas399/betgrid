ad3463cf1bf9a4e24ee2603de07b6e4adf41d012
Wojtas399 <wojtekp26@gmail.com>
2024-11-10 20:39:46 +0100

    SeasonGrandPrix entity.

---
+++ lib/model/season_grand_prix.dart
@@ -0,0 +1,28 @@
+import 'entity.dart';
+
+class SeasonGrandPrix extends Entity {
+  final int season;
+  final String grandPrixId;
+  final int roundNumber;
+  final DateTime startDate;
+  final DateTime endDate;
+
+  const SeasonGrandPrix({
+    required super.id,
+    required this.season,
+    required this.grandPrixId,
+    required this.roundNumber,
+    required this.startDate,
+    required this.endDate,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        season,
+        grandPrixId,
+        roundNumber,
+        startDate,
+        endDate,
+      ];
+}
