09141d307f11f04cac0cdef0affcba0239a44857
Wojtas399 <wojtekp26@gmail.com>
2024-11-12 17:11:04 +0100

    Deleted unused GrandPrixRepository.

--- lib/data/repository/grand_prix/grand_prix_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../../../model/grand_prix.dart';
-
-abstract interface class GrandPrixRepository {
-  Stream<List<GrandPrix>?> getAllGrandPrixesFromSeason(int season);
-
-  Stream<GrandPrix?> getGrandPrixById({
-    required String grandPrixId,
-  });
-}
