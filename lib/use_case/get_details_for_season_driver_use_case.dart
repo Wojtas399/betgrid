bc3a18753af977d60e647c2e3eebac166efd5404
Wojtas399 <wojtekp26@gmail.com>
2024-12-29 17:28:34 +0100

    GetDetailsForSeasonDriverUseCase.

---
+++ lib/use_case/get_details_for_season_driver_use_case.dart
@@ -0,0 +1,40 @@
+import 'package:injectable/injectable.dart';
+import 'package:rxdart/rxdart.dart';
+
+import '../data/repository/driver_personal_data/driver_personal_data_repository.dart';
+import '../data/repository/team_basic_info/team_basic_info_repository.dart';
+import '../model/driver_details.dart';
+import '../model/driver_personal_data.dart';
+import '../model/season_driver.dart';
+import '../model/team_basic_info.dart';
+
+@injectable
+class GetDetailsForSeasonDriverUseCase {
+  final DriverPersonalDataRepository _driverPersonalDataRepository;
+  final TeamBasicInfoRepository _teamBasicInfoRepository;
+
+  const GetDetailsForSeasonDriverUseCase(
+    this._driverPersonalDataRepository,
+    this._teamBasicInfoRepository,
+  );
+
+  Stream<DriverDetails?> call(SeasonDriver seasonDriver) {
+    return Rx.combineLatest2(
+      _driverPersonalDataRepository.getDriverPersonalDataById(
+        seasonDriver.driverId,
+      ),
+      _teamBasicInfoRepository.getTeamBasicInfoById(seasonDriver.teamId),
+      (DriverPersonalData? personalData, TeamBasicInfo? teamBasicInfo) =>
+          personalData != null && teamBasicInfo != null
+              ? DriverDetails(
+                  seasonDriverId: seasonDriver.id,
+                  name: personalData.name,
+                  surname: personalData.surname,
+                  number: seasonDriver.driverNumber,
+                  teamName: teamBasicInfo.name,
+                  teamHexColor: teamBasicInfo.hexColor,
+                )
+              : null,
+    );
+  }
+}
---
+++ test/use_case/get_details_for_season_driver_use_case_test.dart
@@ -0,0 +1,101 @@
+import 'package:betgrid/model/driver_details.dart';
+import 'package:betgrid/model/driver_personal_data.dart';
+import 'package:betgrid/model/season_driver.dart';
+import 'package:betgrid/model/team_basic_info.dart';
+import 'package:betgrid/use_case/get_details_for_season_driver_use_case.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../creator/season_driver_creator.dart';
+import '../creator/team_basic_info_creator.dart';
+import '../mock/data/repository/mock_driver_personal_data_repository.dart';
+import '../mock/data/repository/mock_team_basic_info_repository.dart';
+
+void main() {
+  final driverPersonalDataRepository = MockDriverPersonalDataRepository();
+  final teamBasicInfoRepository = MockTeamBasicInfoRepository();
+  final useCase = GetDetailsForSeasonDriverUseCase(
+    driverPersonalDataRepository,
+    teamBasicInfoRepository,
+  );
+  final SeasonDriver seasonDriver = const SeasonDriverCreator(
+    id: 'sd1',
+    driverId: 'd1',
+    driverNumber: 1,
+  ).createEntity();
+  final DriverPersonalData driverPersonalData = DriverPersonalData(
+    id: seasonDriver.driverId,
+    name: 'Max',
+    surname: 'Verstappen',
+  );
+  final TeamBasicInfo teamBasicInfo = const TeamBasicInfoCreator(
+    name: 'Red Bull Racing',
+    hexColor: '#FFFFFF',
+  ).createEntity();
+  final DriverDetails expectedDriver = DriverDetails(
+    seasonDriverId: seasonDriver.id,
+    name: driverPersonalData.name,
+    surname: driverPersonalData.surname,
+    number: seasonDriver.driverNumber,
+    teamName: teamBasicInfo.name,
+    teamHexColor: teamBasicInfo.hexColor,
+  );
+
+  tearDown(() {
+    verify(
+      () => driverPersonalDataRepository.getDriverPersonalDataById(
+        seasonDriver.driverId,
+      ),
+    ).called(1);
+    verify(
+      () => teamBasicInfoRepository.getTeamBasicInfoById(seasonDriver.teamId),
+    ).called(1);
+    reset(driverPersonalDataRepository);
+    reset(teamBasicInfoRepository);
+  });
+
+  test(
+    'should emit null if personal data of the driver does not exist in '
+    'DriverPersonalDataRepository',
+    () async {
+      driverPersonalDataRepository.mockGetDriverPersonalDataById();
+      teamBasicInfoRepository.mockGetTeamBasicInfoById();
+
+      final Stream<DriverDetails?> driver$ = useCase(seasonDriver);
+
+      expect(await driver$.first, null);
+    },
+  );
+
+  test(
+    'should emit null if team to which driver belongs does not exist in '
+    'TeamRepository',
+    () async {
+      driverPersonalDataRepository.mockGetDriverPersonalDataById(
+        expectedDriverPersonalData: driverPersonalData,
+      );
+      teamBasicInfoRepository.mockGetTeamBasicInfoById();
+
+      final Stream<DriverDetails?> driver$ = useCase(seasonDriver);
+
+      expect(await driver$.first, null);
+    },
+  );
+
+  test(
+    'should emit Driver model which contains data from SeasonDriver, '
+    'DriverPersonalData and Team',
+    () async {
+      driverPersonalDataRepository.mockGetDriverPersonalDataById(
+        expectedDriverPersonalData: driverPersonalData,
+      );
+      teamBasicInfoRepository.mockGetTeamBasicInfoById(
+        expectedTeamBasicInfo: teamBasicInfo,
+      );
+
+      final Stream<DriverDetails?> driver$ = useCase(seasonDriver);
+
+      expect(await driver$.first, expectedDriver);
+    },
+  );
+}
