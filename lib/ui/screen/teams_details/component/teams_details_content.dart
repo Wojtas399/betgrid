654bdcac77d4266f4b58ba00befdf0adeed9c3b3
Wojtas399 <wojtekp26@gmail.com>
2025-04-05 21:08:45 +0200

    UI of card representing a team.

--- lib/ui/screen/teams_details/component/teams_details_content.dart
+++ lib/ui/screen/teams_details/component/teams_details_content.dart
@@ -3,6 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../model/season_team.dart';
 import '../../../component/custom_card_component.dart';
+import '../../../component/text_component.dart';
+import '../../../extensions/string_extensions.dart';
 import '../cubit/teams_details_cubit.dart';
 import '../cubit/teams_details_state.dart';
 
@@ -30,16 +32,45 @@ class _ListOfTeams extends StatelessWidget {
     );
 
     return ListView.builder(
+      padding: const EdgeInsets.all(8),
       itemCount: teams.length,
       itemBuilder: (_, int index) {
         final SeasonTeam team = teams[index];
 
-        return CustomCard(
-          child: Column(
-            children: [Text(team.shortName), Image.network(team.carImgUrl)],
-          ),
-        );
+        return _TeamItem(team: team);
       },
+    );
+  }
+}
+
+class _TeamItem extends StatelessWidget {
+  final SeasonTeam team;
+
+  const _TeamItem({required this.team});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomCard(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 24,
+                height: 24,
+                decoration: BoxDecoration(
+                  color: team.baseHexColor.toColor(),
+                  shape: BoxShape.circle,
+                ),
+              ),
+              const SizedBox(width: 16),
+              TitleMedium(team.shortName),
+            ],
+          ),
+          const SizedBox(height: 16),
+          Image.network(team.carImgUrl),
+        ],
+      ),
     );
   }
 }
